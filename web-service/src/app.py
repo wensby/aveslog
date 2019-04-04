476d3b54cb788859f89547cb1979e99690951999
Lukas J. Wensby <wensby@gmail.com>
2019-04-04 16:09:24 +0200

    Implement fundamentals for login functionality
    You can now login, but all users get the same bird list. And there is currently
    no way of loggin out.

--- web-service/src/app.py
+++ web-service/src/app.py
@@ -1,25 +1,43 @@
 import os.path
 import re
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, session, render_template
 app = Flask(__name__)
 
 bird_filepath_str = '/data/bird.txt'
+app.secret_key = '40958hft09834utnv093nt50934tv983vnu4'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'GET':
+    return render_template('login.html')
+  else:
+    username = request.form['username']
+    session['username'] = username
+    return redirect(url_for('index'))
+
+def putbird(bird):
+  if len(bird) < 20 and re.compile("^([A-z]+)( [A-z]+)*$").match(bird):
+    with open(bird_filepath_str, 'a+') as f:
+      f.write(bird + '\n')
+
+def getbirds():
+  if not os.path.exists(bird_filepath_str):
+    open(bird_filepath_str, 'w+').close()
+  with open(bird_filepath_str, 'r') as f:
+    return [line.rstrip('\n') for line in f]
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
-  if request.method == 'POST':
-    bird = request.form['bird']
-    if len(bird) < 20 and re.compile("^([A-z]+)( [A-z]+)*$").match(bird):
-      with open(bird_filepath_str, 'a+') as f:
-        f.write(bird + '\n')
-    return redirect(url_for('hello_world'))
+def index():
+  if 'username' in session:
+    if request.method == 'POST':
+      bird = request.form['bird']
+      putbird(bird)
+      return redirect(url_for('index'))
+    else:
+      birds = getbirds()
+      return render_template('home.html', username=session['username'], birds=birds)
   else:
-    form = '<form action="/" method="post">Bird:<input type="text" name="bird"></form><br>'
-    if not os.path.exists(bird_filepath_str):
-      open(bird_filepath_str, 'w+').close()
-    with open(bird_filepath_str, 'r') as f:
-      birds = [line.rstrip('\n') for line in f]
-      return form + repr(birds)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3002)
