e39e778e00a537ef14737ec54e13a877898a9f9a
Lukas J. Wensby <wensby@gmail.com>
2019-05-02 19:20:31 +0900

    Let all pages extend base.html

--- database/schema/public/table/sighting.sql
+++ database/schema/public/table/sighting.sql
@@ -2,9 +2,10 @@ CREATE TABLE sighting (
   id SERIAL,
   person_id INTEGER,
   bird_id INTEGER,
-  sighting_time TIMESTAMP WITHOUT TIME ZONE,
+  sighting_date DATE,
+  sighting_time TIME WITHOUT TIME ZONE,
   CONSTRAINT sighting_id_primary_key PRIMARY KEY (id),
   FOREIGN KEY (person_id) REFERENCES person (id),
   FOREIGN KEY (bird_id) REFERENCES bird (id),
-  CONSTRAINT sighting_time_not_null CHECK (sighting_time IS NOT NULL)
+  CONSTRAINT sighting_date_not_null CHECK (sighting_date IS NOT NULL)
 );
