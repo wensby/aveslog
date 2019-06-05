920fe73b9f98c16be7a479b4175bc17cb0c3e359
Lukas J. Wensby <wensby@gmail.com>
2019-06-05 14:43:04 +0900

    Limit shown search result items to 100
    Some queries produced insane quantities of result items, causing the page to
    load so slowly that it became unresponsive and took a lot of time before the
    user could expand the menu and navigate away.

--- web-service/src/birding/blueprint_search.py
+++ web-service/src/birding/blueprint_search.py
@@ -12,7 +12,7 @@ def create_search_blueprint(bird_searcher, bird_search_view_factory):
   @blueprint.route('/bird/search')
   def bird_search():
     name = request.args.get('query')
-    bird_matches = bird_searcher.search(name)
+    bird_matches = bird_searcher.search(name)[:100]
     result_items = view_factory.create_search_result_items(bird_matches)
     g.render_context['result_items'] = result_items
     return render_page('birdsearch.html')
