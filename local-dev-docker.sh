2b9471abd18e7a9831b4c3a992eb47abcbd9c1c8
Lukas J. Wensby <wensby@gmail.com>
2019-06-08 11:00:50 +0900

    Adjust locale dev script to target one service
    Now you can give a service name as argument to the locale dev script to rebuild
    and restart only that one.

--- local-dev-docker.sh
+++ local-dev-docker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-docker-compose \
-  -f docker-compose.yml \
-  -f docker-compose.dev.yml \
-  up -d --no-deps --build --force-recreate
+if [ -z "$1" ]; then
+  docker-compose \
+    -f docker-compose.yml \
+    -f docker-compose.dev.yml \
+    up -d --no-deps --build --force-recreate
+else
+  docker-compose \
+    -f docker-compose.yml \
+    -f docker-compose.dev.yml \
+    up -d --no-deps --build --force-recreate $1
+fi
