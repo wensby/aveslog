a6202fa58ef5c1795089f10f6ee07dae216b4fe5
Lukas J. Wensby <wensby@gmail.com>
2019-06-23 22:16:59 +0900

    Improve test script speed
    Now, the virtual environment will only be created and setup if you run the
    script using the -s|--setup flag. This has greatly improved the speed.

--- test.sh
+++ test.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-python3.7 -m venv web-service/venv
+# Parse Options
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -s|--setup) # Setup environment, used first time, or on changes
+      SETUP="$1"
+      shift # past argument
+      ;;
+    *)
+      POSITIONAL+=("$1")
+      shift # past argument
+      ;;
+  esac
+done
+
+if ! [ -z "$SETUP" ]; then
+  python3.7 -m venv web-service/venv
+fi
+
 source web-service/venv/bin/activate
-pip install -q --upgrade pip
-pip install -q -r web-service/requirements.txt
+
+if ! [ -z "$SETUP" ]; then
+  pip install -q --upgrade pip
+  pip install -q -r web-service/requirements.txt
+fi
+
 cd web-service/src/birding
 python -m unittest discover -t ..
 deactivate
