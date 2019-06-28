c95250113ae1f812c4e6005830f8a3e615ebb5fa
Lukas J. Wensby <wensby@gmail.com>
2019-06-28 23:20:45 +0900

    Reformat test_authentication_blueprint.py

--- web-service/src/tests/test_authentication_blueprint.py
+++ web-service/src/tests/test_authentication_blueprint.py
@@ -10,6 +10,7 @@ class TestAuthenticationBlueprint(AppTestCase):
 
   def test_get_register_request_contains_bird_search(self):
     response = self.client.get(url_for('authentication.get_register_request'))
+
     self.assertEqual(response.status_code, HTTPStatus.OK)
     html = HTML(html=response.data)
     form = html.find('form#birdSearchForm', first=True)
@@ -21,6 +22,7 @@ class TestAuthenticationBlueprint(AppTestCase):
 
   def test_get_register_request_contains_email_submit_form(self):
     response = self.client.get(url_for('authentication.get_register_request'))
+
     self.assertEqual(response.status_code, HTTPStatus.OK)
     html = HTML(html=response.data)
     form = html.find("form[method='post']", first=True)
@@ -30,6 +32,7 @@ class TestAuthenticationBlueprint(AppTestCase):
 
   def test_get_register_request_contains_link_back_to_login(self):
     response = self.client.get(url_for('authentication.get_register_request'))
+
     self.assertEqual(response.status_code, HTTPStatus.OK)
     html = HTML(html=response.data)
     self.assertIn(url_for('authentication.get_login'), html.links)
@@ -37,7 +40,9 @@ class TestAuthenticationBlueprint(AppTestCase):
   def test_get_register_request_redirect_when_logged_in(self):
     self.db_insert_account(4)
     self.populate_session('account_id', 4)
+
     response = self.client.get(url_for('authentication.get_register_request'))
+
     self.assertEqual(response.status_code, HTTPStatus.FOUND)
     html = HTML(html=response.data)
     self.assertListEqual(list(html.links), [url_for('home.index')])
