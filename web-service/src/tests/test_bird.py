95fe18e087333bdd10f68d1c4646ee03c7fb4602
Lukas J. Wensby <wensby@gmail.com>
2019-06-28 23:46:20 +0900

    Add BirdThumbnail class unit tests

--- web-service/src/tests/test_bird.py
+++ web-service/src/tests/test_bird.py
@@ -1,9 +1,15 @@
 from unittest import TestCase
 
-from birding.bird import Bird
+from birding.bird import Bird, BirdThumbnail
 
 
 class TestBird(TestCase):
 
   def test_construction(self):
     Bird(4, 'Pica pica')
+
+
+class TestBirdThumbnail(TestCase):
+
+  def test_construction(self):
+    BirdThumbnail(4, 8)
