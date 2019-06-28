d240ea983e9e25385120c9e43433975f212fd559
Lukas J. Wensby <wensby@gmail.com>
2019-06-28 17:01:49 +0900

    Only measure coverage of production code
    There is not much value of spamming the coverage report with coverage data of
    the actual test code.

--- web-service/run_tests.sh
+++ web-service/run_tests.sh
@@ -5,7 +5,7 @@ while ! nc -z test-database-service 5432; do sleep 1; done
 if [ -n "$1" ]; then
   rm .coverage
   rm -rf htmlcov
-  coverage run -m unittest discover -s tests
+  coverage run --source birding -m unittest discover -s tests
   coverage html
 else
   python -m unittest discover -s tests
