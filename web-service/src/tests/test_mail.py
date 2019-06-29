52822b0dc4c98b8dd2561e4691b59192a34ddfcf
Lukas J. Wensby <wensby@gmail.com>
2019-06-30 00:32:28 +0900

    Try to cover MailServerDispatcher with unit tests

--- web-service/src/tests/test_mail.py
+++ web-service/src/tests/test_mail.py
@@ -1,6 +1,8 @@
+import os
 from unittest import TestCase
 
-from birding.mail import EmailAddress
+import birding
+from birding.mail import EmailAddress, MailServerDispatcher
 
 
 class TestEmailAddress(TestCase):
@@ -14,3 +16,42 @@ class TestEmailAddress(TestCase):
   def test_repr(self):
     self.assertEqual(
       repr(EmailAddress('my@mail.com')), 'EmailAddress<my@mail.com>')
+
+
+class TestMailDispatcherFactory(TestCase):
+
+  def setUp(self) -> None:
+    os.environ['MAIL_SERVER'] = 'myMailServer'
+    os.environ['MAIL_PORT'] = 'myMailPort'
+    os.environ['MAIL_USERNAME'] = 'myMailUsername'
+    os.environ['MAIL_PASSWORD'] = 'myMailPassword'
+    os.environ['MAIL_USE_TLS'] = 'false'
+    os.environ['MAIL_USE_SSL'] = 'true'
+
+  def test_app_construction_with_mail_server_environment_variables(self):
+    test_config = {'TESTING': True, 'SECRET_KEY': 'wowsosecret'}
+    birding.create_app(test_config=test_config)
+
+  def tearDown(self) -> None:
+    del os.environ['MAIL_SERVER']
+    del os.environ['MAIL_PORT']
+    del os.environ['MAIL_USERNAME']
+    del os.environ['MAIL_PASSWORD']
+    del os.environ['MAIL_USE_TLS']
+    del os.environ['MAIL_USE_SSL']
+
+class TestMailServerDispatcher(TestCase):
+
+  def setUp(self) -> None:
+    test_config = {'TESTING': True, 'SECRET_KEY': 'wowsosecret'}
+    app = birding.create_app(test_config=test_config)
+    self.dispatcher = MailServerDispatcher(app, None, None, 'myUsername', None, None, None)
+    self.app_context = app.test_request_context()
+    self.app_context.push()
+
+  def test_dispatch(self):
+    email_address = EmailAddress('my@email.com')
+    self.dispatcher.dispatch(email_address, 'mySubject', 'myBody')
+
+  def tearDown(self) -> None:
+    self.app_context.pop()
