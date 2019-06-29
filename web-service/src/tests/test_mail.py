2a1d67e4fa17cc4def6fef6c117d244261d55641
Lukas J. Wensby <wensby@gmail.com>
2019-06-29 15:46:08 +0900

    Add EmailAddress unit test

--- web-service/src/tests/test_mail.py
+++ web-service/src/tests/test_mail.py
@@ -7,3 +7,6 @@ class TestEmailAddress(TestCase):
 
   def test_construction_when_invalid_format(self):
     self.assertRaises(Exception, EmailAddress, *('invalidformat',))
+
+  def test_eq_false_when_different_type(self):
+    self.assertNotEqual(EmailAddress('my@email.com'), 'my@email.com')
