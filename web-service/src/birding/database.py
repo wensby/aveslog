fc03b7057fd2f39e43f690b97bcdcf21bbf63f96
Lukas J. Wensby <wensby@gmail.com>
2019-06-30 13:47:29 +0900

    Add transaction method to Database
    This will allow for executing a lot of queries within a single transaction by
    using the pattern:
    
    with.database.transaction() as transaction:
      result = transaction.execute('...')
      ...
      result = transaction.execute('...', ('vars',))
      ...

--- web-service/src/birding/database.py
+++ web-service/src/birding/database.py
@@ -22,6 +22,9 @@ class Database:
     self.logger = logger
     self.connection_pool = connection_pool
 
+  def transaction(self):
+    return Transaction(self.connection_pool)
+
   def query(self, query, vars=None):
     connection = self.__get_connection()
     cursor = connection.cursor()
@@ -42,6 +45,34 @@ class Database:
   def __get_connection(self):
     self.logger.info('Getting database connection')
     return self.connection_pool.getconn()
+
+class Transaction:
+
+  def __init__(self, connection_pool):
+    self.connection_pool = connection_pool
+
+  def __enter__(self):
+    self.connection = self.__get_connection()
+    self.cursor = self.connection.cursor()
+    return self
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    self.connection.commit()
+    self.cursor.close()
+    self.connection_pool.putconn(self.connection)
+
+  @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
+  def __get_connection(self):
+    return self.connection_pool.getconn()
+
+  def execute(self, query, vars=None):
+    self.cursor.execute(query, vars)
+    try:
+      rows = self.cursor.fetchall()
+    except:
+      rows = []
+    status = self.cursor.statusmessage
+    return QueryResult(status, rows)
 
 class QueryResult:
 
