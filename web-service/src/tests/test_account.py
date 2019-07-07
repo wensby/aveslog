88a26d6fc1aeee4952c861f22a84d01c38ff2bc4
Lukas J. Wensby <wensby@gmail.com>
2019-07-07 21:50:41 +0900

    Reformat test_account.py

--- web-service/src/tests/test_account.py
+++ web-service/src/tests/test_account.py
@@ -12,6 +12,7 @@ from birding.mail import EmailAddress
 
 from test_util import mock_database_transaction
 
+
 class TestAccountRepository(TestCase):
 
   def setUp(self):
@@ -19,15 +20,17 @@ class TestAccountRepository(TestCase):
     self.password_hasher = Mock(spec=PasswordHasher)
     self.token_factory = Mock(spec=TokenFactory)
     self.repository = AccountRepository(
-        self.database,
-        self.password_hasher,
-        self.token_factory,
+      self.database,
+      self.password_hasher,
+      self.token_factory,
     )
 
   def test_find_account_by_id_queries_database_correctly(self):
     self.database.query().rows = []
     result = self.repository.find_account_by_id(4)
-    self.database.query.assert_called_with('SELECT id, username, email, person_id, locale_id FROM user_account WHERE id = %s;', (4,))
+    self.database.query.assert_called_with(
+      'SELECT id, username, email, person_id, locale_id FROM user_account WHERE id = %s;',
+      (4,))
 
   def test_find_account_by_id_parses_account_correctly_when_present(self):
     self.database.query().rows = [[4, 'username', 'e@mail.com', 8, 15]]
@@ -37,18 +40,24 @@ class TestAccountRepository(TestCase):
   def test_create_account_registration_queries_database_correctly(self):
     email = EmailAddress('e@mail.com')
     token = self.token_factory.create_token()
-    self.database.query.side_effect = [Simple(rows=[[4, 'e@mail.com', 'myToken']])]
+    self.database.query.side_effect = [
+      Simple(rows=[[4, 'e@mail.com', 'myToken']])]
 
     self.repository.create_account_registration(email)
 
     self.database.query.assert_has_calls([
-        call('INSERT INTO user_account_registration (email, token) VALUES (%s, %s) RETURNING id, email, token;', (email.raw, token)),
+      call(
+        'INSERT INTO user_account_registration (email, token) VALUES (%s, %s) RETURNING id, email, token;',
+        (email.raw, token)),
     ])
 
   def test_find_account_registration_by_token_queries_database_correctly(self):
     self.database.query().rows = []
     self.repository.find_account_registration_by_token('myToken')
-    self.database.query.assert_called_with('SELECT id, email, token FROM user_account_registration WHERE token LIKE %s;', ('myToken',))
+    self.database.query.assert_called_with(
+      'SELECT id, email, token FROM user_account_registration WHERE token LIKE %s;',
+      ('myToken',))
+
 
 class TestAccountFactory(TestCase):
 
@@ -85,15 +94,18 @@ class TestPasswordHasher(TestCase):
     password = 'myPassword'
     self.salt_factory.create_salt.return_value = salt
 
-    result = self.password_hasher.create_salt_hashed_password(Password(password))
+    result = self.password_hasher.create_salt_hashed_password(
+      Password(password))
 
-    expected_hashed_password = hexlify(pbkdf2_hmac('sha256', password.encode(), salt.encode(), 100000)).decode()
+    expected_hashed_password = hexlify(
+      pbkdf2_hmac('sha256', password.encode(), salt.encode(), 100000)).decode()
     self.assertEqual(result, (salt, expected_hashed_password))
 
   def test_hash_password(self):
     hasher = PasswordHasher(Mock())
     hash = '0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5'
     self.assertTrue(hasher.hash_password(Password('password'), 'salt') == hash)
+
 
 class TestPasswordRepository(TestCase):
 
@@ -103,30 +115,34 @@ class TestPasswordRepository(TestCase):
     self.password_hasher = Mock()
     self.salt_factory = Mock()
     self.repository = PasswordRepository(
-        self.token_factory,
-        self.database,
-        self.password_hasher,
+      self.token_factory,
+      self.database,
+      self.password_hasher,
     )
 
   def test_create_password_reset_token_queries_database_correctly(self):
     account = Simple(id=1)
     self.token_factory.create_token.return_value = 'myToken'
     self.database.query.side_effect = [
-        Simple(status='INSERT'),
-        Simple(rows=[[1, 'some token, hopefully the just inserted one']])
+      Simple(status='INSERT'),
+      Simple(rows=[[1, 'some token, hopefully the just inserted one']])
     ]
 
     self.repository.create_password_reset_token(account)
 
-    insert_call = call('INSERT INTO password_reset_token (user_account_id, token) VALUES (%s, %s) ON CONFLICT (user_account_id) DO UPDATE SET token = excluded.token;', (1, 'myToken'))
-    select_call = call('SELECT user_account_id, token FROM password_reset_token WHERE user_account_id = %s;', (account.id,))
+    insert_call = call(
+      'INSERT INTO password_reset_token (user_account_id, token) VALUES (%s, %s) ON CONFLICT (user_account_id) DO UPDATE SET token = excluded.token;',
+      (1, 'myToken'))
+    select_call = call(
+      'SELECT user_account_id, token FROM password_reset_token WHERE user_account_id = %s;',
+      (account.id,))
     self.database.query.assert_has_calls([insert_call, select_call])
 
   def test_create_password_reset_token_returns_selected_token(self):
     account = Simple(id=1)
     self.database.query.side_effect = [
-        Simple(status='INSERT'),
-        Simple(rows=[[1, 'selectedToken']])
+      Simple(status='INSERT'),
+      Simple(rows=[[1, 'selectedToken']])
     ]
 
     result = self.repository.create_password_reset_token(account)
@@ -136,8 +152,8 @@ class TestPasswordRepository(TestCase):
   def test_create_password_reset_token_replaces_previously_existing_token(self):
     account = Simple(id=1)
     self.database.query.side_effect = [
-        Simple(status='INSERT'),
-        Simple(rows=[[1, 'selectedToken']])
+      Simple(status='INSERT'),
+      Simple(rows=[[1, 'selectedToken']])
     ]
 
     result = self.repository.create_password_reset_token(account)
@@ -150,12 +166,16 @@ class TestPasswordRepository(TestCase):
 
     self.repository.find_password_reset_token(account)
 
-    self.database.query.assert_called_with('SELECT user_account_id, token FROM password_reset_token WHERE user_account_id = %s;', (account.id,))
+    self.database.query.assert_called_with(
+      'SELECT user_account_id, token FROM password_reset_token WHERE user_account_id = %s;',
+      (account.id,))
 
   def test_find_password_reset_account_id_queries_database_correctly(self):
     self.database.query.return_value = Simple(rows=[])
     self.repository.find_password_reset_account_id('myToken')
-    self.database.query.assert_called_with('SELECT user_account_id FROM password_reset_token WHERE token LIKE %s;', ('myToken',))
+    self.database.query.assert_called_with(
+      'SELECT user_account_id FROM password_reset_token WHERE token LIKE %s;',
+      ('myToken',))
 
   def test_find_password_reset_account_id_parses_response_correctly(self):
     self.database.query.return_value = Simple(rows=[[4]])
@@ -163,10 +183,14 @@ class TestPasswordRepository(TestCase):
     self.assertEqual(result, 4)
 
   def test_update_password_queries_database_correctly(self):
-    self.password_hasher.create_salt_hashed_password.return_value = ('mySalt', 'myHashedPassword')
+    self.password_hasher.create_salt_hashed_password.return_value = (
+    'mySalt', 'myHashedPassword')
     self.repository.update_password(4, 'myNewPassword')
-    self.database.query.assert_called_with('UPDATE hashed_password SET salt = %s, salted_hash = %s WHERE user_account_id = %s;', ('mySalt', 'myHashedPassword', 4))
+    self.database.query.assert_called_with(
+      'UPDATE hashed_password SET salt = %s, salted_hash = %s WHERE user_account_id = %s;',
+      ('mySalt', 'myHashedPassword', 4))
 
   def test_remove_password_reset_token_queries_database_correctly(self):
     self.repository.remove_password_reset_token('myToken')
-    self.database.query.assert_called_with('DELETE FROM password_reset_token WHERE token LIKE %s;', ('myToken',))
+    self.database.query.assert_called_with(
+      'DELETE FROM password_reset_token WHERE token LIKE %s;', ('myToken',))
