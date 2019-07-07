38473d50df4182c1e11b8e8be5eff6104d869fba
Lukas J. Wensby <wensby@gmail.com>
2019-07-07 17:11:12 +0900

    Reformat bird_blueprint.py

--- web-service/src/birding/bird_blueprint.py
+++ web-service/src/birding/bird_blueprint.py
@@ -3,7 +3,9 @@ from flask import request
 from flask import g
 from .render import render_page
 
-def create_bird_blueprint(bird_view_factory, bird_search_controller, bird_search_view_factory):
+
+def create_bird_blueprint(bird_view_factory, bird_search_controller,
+      bird_search_view_factory):
   controller = bird_search_controller
   view_factory = bird_view_factory
 
@@ -20,7 +22,8 @@ def create_bird_blueprint(bird_view_factory, bird_search_controller, bird_search
   def search():
     name = request.args.get('query')
     bird_matches = controller.search(name)
-    result_items = bird_search_view_factory.create_search_result_items(bird_matches)
+    result_items = bird_search_view_factory.create_search_result_items(
+      bird_matches)
     g.render_context['result_items'] = result_items
     return render_page('birdsearch.html')
 
