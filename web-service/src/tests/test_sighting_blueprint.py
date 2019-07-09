e6c8fbc11c192ccc23af05056da6f3c7385a3bb8
Lukas J. Wensby <wensby@gmail.com>
2019-07-09 18:41:26 +0900

    Move a couple of tests

--- web-service/src/tests/test_sighting_blueprint.py
+++ web-service/src/tests/test_sighting_blueprint.py
@@ -28,6 +28,19 @@ class TestSightingBlueprint(AppTestCase):
     html = HTML(html=response.data)
     self.assertFalse(html.find('.card'))
 
+
+class TestCreateSightingForm(AppTestCase):
+
+  def test_get_create_ok(self):
+    self.db_insert_bird(4, 'Pica pica')
+    self.db_insert_person(8)
+    self.db_insert_account(15, 'myUsername', 'my@email.com', 8, None)
+    self.set_logged_in(15)
+
+    response = self.client.get(url_for('sighting.create', birdid=4))
+
+    self.assertEqual(response.status_code, HTTPStatus.OK)
+
   def test_post_sighting_redirects_to_login_when_logged_out(self):
     response = self.__post_sighting(4, '2008-01-05')
     self.assertRedirect(response, 'authentication.get_login')
@@ -45,16 +58,3 @@ class TestSightingBlueprint(AppTestCase):
   def __post_sighting(self, bird_id, dateInput):
     data = {'birdId': bird_id, 'dateInput': dateInput}
     return self.client.post(url_for('sighting.post_sighting'), data=data)
-
-
-class TestCreateSightingForm(AppTestCase):
-
-  def test_get_create_ok(self):
-    self.db_insert_bird(4, 'Pica pica')
-    self.db_insert_person(8)
-    self.db_insert_account(15, 'myUsername', 'my@email.com', 8, None)
-    self.set_logged_in(15)
-
-    response = self.client.get(url_for('sighting.create', birdid=4))
-
-    self.assertEqual(response.status_code, HTTPStatus.OK)
