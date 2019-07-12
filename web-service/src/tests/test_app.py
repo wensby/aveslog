d1d9b27e0b3d7c6e13c7c9b1239370b51efde76b
Lukas J. Wensby <wensby@gmail.com>
2019-07-12 17:46:56 +0900

    Unit test config reading on app creation

--- web-service/src/tests/test_app.py
+++ web-service/src/tests/test_app.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from unittest import TestCase
 
 import birding
@@ -8,3 +10,19 @@ class TestAppCreation(TestCase):
   def test_creation(self):
     test_config = {'TESTING': True, 'SECRET_KEY': 'wowsosecret'}
     birding.create_app(test_config=test_config)
+
+  def test_creation_creates_instance_directory(self):
+    shutil.rmtree('instance')
+    test_config = {'TESTING': True, 'SECRET_KEY': 'wowsosecret'}
+    birding.create_app(test_config=test_config)
+    self.assertIn('instance', os.listdir('.'))
+
+  def test_creation_fails_if_no_secret_key(self):
+    test_config = {'TESTING': True}
+    self.assertRaises(Exception, birding.create_app, test_config=test_config)
+
+  def test_creation_reads_from_instance_config(self):
+    with open('instance/config.py', 'w') as file:
+      file.writelines(["SECRET_KEY = 'wowsosecret'\n", "TESTING = True\n"])
+    self.assertTrue(os.path.exists('instance/config.py'))
+    birding.create_app()
