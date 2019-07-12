2c8e73c6906bc57c6262fb3eb0a96ad889538a05
Lukas J. Wensby <wensby@gmail.com>
2019-07-12 17:28:13 +0900

    Clean up test_app module

--- web-service/src/tests/test_app.py
+++ web-service/src/tests/test_app.py
@@ -7,4 +7,4 @@ class TestAppCreation(TestCase):
 
   def test_creation(self):
     test_config = {'TESTING': True, 'SECRET_KEY': 'wowsosecret'}
-    app = birding.create_app(test_config=test_config)
+    birding.create_app(test_config=test_config)
