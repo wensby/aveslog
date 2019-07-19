f785a782949fc30d1154c463e8f27b4d11c5dcaf
Lukas J. Wensby <wensby@gmail.com>
2019-07-19 19:15:43 +0900

    Reformat authentication_blueprint.py

--- web-service/src/birding/authentication_blueprint.py
+++ web-service/src/birding/authentication_blueprint.py
@@ -11,6 +11,7 @@ from .account import Credentials
 from .account import Username
 from .account import Password
 
+
 def require_login(view):
   @wraps(view)
   def wrapped_view(**kwargs):
@@ -18,7 +19,9 @@ def require_login(view):
       return view(**kwargs)
     else:
       return redirect(url_for('authentication.get_login'))
+
   return wrapped_view
+
 
 def require_logout(view):
   @wraps(view)
@@ -27,24 +30,31 @@ def require_logout(view):
       return redirect(url_for('home.index'))
     else:
       return view(**kwargs)
+
   return wrapped_view
 
-def create_authentication_blueprint(account_repository, authenticator, account_registration_controller, password_reset_controller):
-  blueprint = Blueprint('authentication', __name__, url_prefix='/authentication')
+
+def create_authentication_blueprint(account_repository, authenticator,
+      account_registration_controller, password_reset_controller):
+  blueprint = Blueprint('authentication', __name__,
+                        url_prefix='/authentication')
 
   @blueprint.route('/register')
   @require_logout
   def get_register_request():
     return render_page('registration_request.html')
-  
+
   @blueprint.route('/register', methods=['POST'])
   @require_logout
   def post_register_request():
     email = request.form['email']
-    result = account_registration_controller.initiate_registration(email, g.locale)
-    flash(g.locale.text(u'An email containing your registration form link has been sent to your email address.'), 'success')
+    result = account_registration_controller.initiate_registration(email,
+                                                                   g.locale)
+    flash(g.locale.text(
+      u'An email containing your registration form link has been sent to your email address.'),
+          'success')
     return redirect(url_for('authentication.get_register_request'))
-  
+
   @blueprint.route('/register/<token>')
   @require_logout
   def get_register_form(token):
@@ -53,20 +63,23 @@ def create_authentication_blueprint(account_repository, authenticator, account_r
       g.render_context['user_account_registration'] = registration
       return render_page('register.html')
     else:
-      flash(g.locale.text('This registration link is no longer valid, please request a new one.'))
+      flash(g.locale.text(
+        'This registration link is no longer valid, please request a new one.'))
       return redirect(url_for('authentication.get_register_request'))
-  
+
   @blueprint.route('/register/<token>', methods=['POST'])
   @require_logout
   def post_register_form(token):
     if not token == request.form['token']:
-      flash(g.locale.text('Account registraion failure: Registration token discrepancy'), 'danger')
+      flash(g.locale.text(
+        'Account registraion failure: Registration token discrepancy'),
+            'danger')
       return redirect(url_for('authentication.get_register_form', token=token))
     response = account_registration_controller.perform_registration(
-        request.form['email'],
-        request.form['token'],
-        request.form['username'],
-        request.form['password']
+      request.form['email'],
+      request.form['token'],
+      request.form['username'],
+      request.form['password']
     )
     if response == 'associated registration missing':
       flash(g.locale.text('Registration form no longer valid'), 'danger')
@@ -88,7 +101,9 @@ def create_authentication_blueprint(account_repository, authenticator, account_r
   def post_password_reset_link_request():
     email = request.form['email']
     password_reset_controller.initiate_password_reset(email, g.locale)
-    flash(g.locale.text(u"An email has been sent. If you have't received it in a few minutes, please check your spam folder."), 'success')
+    flash(g.locale.text(
+      u"An email has been sent. If you have't received it in a few minutes, please check your spam folder."),
+          'success')
     return redirect(url_for('authentication.get_password_reset_link_request'))
 
   @blueprint.route('/password-reset/<token>')
@@ -108,20 +123,21 @@ def create_authentication_blueprint(account_repository, authenticator, account_r
       return redirect(url_for('authentication.get_login'))
     else:
       flash(u"Something went wrong", 'danger')
-      return redirect(url_for('authentication.get_password_reset_form', token=token))
-
+      return redirect(
+        url_for('authentication.get_password_reset_form', token=token))
 
   @blueprint.route('/login')
   @require_logout
   def get_login():
     return render_page('login.html')
-  
+
   @blueprint.route('/login', methods=['POST'])
   @require_logout
   def post_login():
     posted_username = request.form['username']
     posted_password = request.form['password']
-    if Username.is_valid(posted_username) and Password.is_valid(posted_password):
+    if Username.is_valid(posted_username) and Password.is_valid(
+          posted_password):
       username = Username(posted_username)
       password = Password(posted_password)
       credentials = Credentials(username, password)
@@ -130,7 +146,7 @@ def create_authentication_blueprint(account_repository, authenticator, account_r
         session['account_id'] = account.id
         return redirect(url_for('home.index'))
     return redirect(url_for('authentication.get_login'))
-  
+
   @blueprint.route('/logout')
   def logout():
     session.pop('account_id', None)
