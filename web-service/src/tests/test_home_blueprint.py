b772e8ec8766082a462ad5df522a5d9a0838e1ca
Lukas J. Wensby <wensby@gmail.com>
2019-07-19 19:25:17 +0900

    Combine two unit tests into one

--- web-service/src/tests/test_home_blueprint.py
+++ web-service/src/tests/test_home_blueprint.py
@@ -1,20 +1,13 @@
-from http import HTTPStatus
 from flask import url_for
 from test_util import AppTestCase
-from requests_html import HTML
 
 
 class TestHomePage(AppTestCase):
 
-  def test_get_home_contains_login_link_when_logged_out(self):
+  def test_page_contains_expected_content_when_logged_out(self):
     response = self.client.get(url_for('home.index'))
-    self.assertEqual(response.status_code, HTTPStatus.OK)
-    html = HTML(html=response.data)
+    html = self.assertOkHtmlResponse(response)
     self.assertIn(url_for('authentication.get_login'), html.links)
-
-  def test_get_index_contains_search_form(self):
-    response = self.client.get(url_for('home.index'))
-    html = HTML(html=response.data)
     bird_search_form = html.find('form[action="/bird/search"]', first=True)
     self.assertTrue(bird_search_form)
     self.assertEqual(len(bird_search_form.find('input[name="query"]')), 1)
