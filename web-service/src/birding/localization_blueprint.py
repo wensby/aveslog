6e966c7bbd8f03509763494a5d6f11d60f0e4cc7
Lukas J. Wensby <wensby@gmail.com>
2019-07-19 19:17:29 +0900

    Clean up localization_blueprint.py

--- web-service/src/birding/localization_blueprint.py
+++ web-service/src/birding/localization_blueprint.py
@@ -1,21 +1,22 @@
-import os
-
 from flask import Blueprint, request, redirect, url_for, after_this_request, g
 
+from birding.account import AccountRepository
 from birding.localization import LocaleRepository, LoadedLocale, Locale, \
   LocaleLoader
-from birding.account import AccountRepository
+
 
 def update_locale_context(user_locale_cookie_key, loaded_locale: LoadedLocale):
   previously_set_code = request.cookies.get(user_locale_cookie_key, None)
   # when the response exists, set a cookie with the language if it is new
   if loaded_locale.locale.code and loaded_locale.locale.code is not previously_set_code:
-    set_locale_cookie_after_this_request(loaded_locale.locale, user_locale_cookie_key)
+    set_locale_cookie_after_this_request(loaded_locale.locale,
+                                         user_locale_cookie_key)
   g.locale = loaded_locale
   g.render_context['locale'] = loaded_locale
 
 
-def set_locale_cookie_after_this_request(locale: Locale, user_locale_cookie_key):
+def set_locale_cookie_after_this_request(locale: Locale,
+      user_locale_cookie_key):
   @after_this_request
   def set_locale_cookie(response):
     response.set_cookie(user_locale_cookie_key, locale.code)
