53eac46a873ff19ee485492c4581a70300da27aa
Lukas J. Wensby <wensby@gmail.com>
2019-08-11 23:01:30 +0900

    Refactor Authentication.login() in React frontend

--- react-web-service/src/authentication/authentication.js
+++ react-web-service/src/authentication/authentication.js
@@ -1,9 +1,9 @@
 export default class Authentication {
 
   async login(username, password) {
-    const url = `${window._env_.API_URL}/v2/authentication/token`
-      + `?username=${username}&password=${password}`
-    const response = await fetch(url);
+    const url = `${window._env_.API_URL}/v2/authentication/token`;
+    const parameters = `?username=${username}&password=${password}`;
+    const response = await fetch(url + parameters);
     const jsonResponse = await response.json();
     localStorage.setItem('authToken', jsonResponse.authToken);
   }
