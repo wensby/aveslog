313d461599d32ad4e097819b4b69864f733c3974
Lukas J. Wensby <wensby@gmail.com>
2019-08-25 14:49:37 +0900

    Add bird name translations to React frontend
    Now, a bird's binomial name can be translated to the locale bird name using the
    bird namespace in the i18n t function.

---
+++ react-web-service/src/bird/BirdQueryResult.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import queryString from 'query-string';
+import BirdService from './BirdService.js';
+import { withTranslation } from 'react-i18next';
+
+class BirdQueryResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      authenticated: false,
+      resultItems: [],
+    };
+    this.birdService = new BirdService();
+  }
+  componentDidMount() {
+    const query = this.getLocationQuery(this.props);
+    this.doSearch(query);
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const oldQuery = prevState.query;
+    const newQuery = this.getLocationQuery(this.props);
+    if (newQuery != oldQuery) {
+      this.doSearch(newQuery);
+    }
+  }
+  async doSearch(query) {
+    const response = await this.birdService.queryBirds(query);
+    if (response.status === 'success') {
+      const result = response.result;
+      this.setState({
+        query: query,
+        resultItems: result,
+      });
+    }
+  }
+  getLocationQuery(props) {
+    const { location } = props;
+    return queryString.parse(location.search).q;
+  }
+  renderItemPicture = item => {
+    if (item.thumbnail) {
+      return <img style={{ maxHeight: '150px' }} src={item.thumbnail} alt="Card image" />;
+    }
+    return (<img style={{ maxHeight: '150px' }} src='/placeholder-bird.png' alt="Card image cap" />);
+  };
+  renderItemName = item => {
+    const { t } = this.props;
+    return [
+      <h5 key='1' className="card-title">{t(`bird:${item.binomialName}`)}</h5>,
+      <h6 key='2' className="card-subtitle mb-2 text-muted">{item.binomialName}</h6>
+    ];
+  };
+  renderAddSightingLink = item => {
+    const { t } = this.props;
+    const { authenticated } = this.state;
+    if (authenticated) {
+      return <Link to='/' className="btn btn-primary">
+        {t('Add new sighting')}
+      </Link>;
+    }
+    else {
+      return null;
+    }
+  };
+  renderItem = (item, index) => {
+    return (<div key={index} className="card">
+      <div className="card-horizontal">
+        <div className="img-square-wrapper">
+          <Link to='/'>
+            {this.renderItemPicture(item)}
+          </Link>
+        </div>
+        <div className="card-body">
+          {this.renderItemName(item)}
+          {this.renderAddSightingLink(item)}
+        </div>
+      </div>
+    </div>);
+  };
+  renderItems = () => {
+    const { resultItems } = this.state;
+    return resultItems.map(this.renderItem);
+  };
+  render() {
+    return (<div className="text-break">
+      {this.renderItems()}
+    </div>);
+  }
+}
+
+export default withTranslation()(BirdQueryResult);
--- react-web-service/src/i18n.js
+++ react-web-service/src/i18n.js
@@ -2,18 +2,24 @@ import i18n from "i18next";
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import i18next from "i18next";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    ns: ['translation', 'bird'],
+    defaultNS: 'translation',
     fallbackLng: 'en',
     keySeparator: false,
     debug: true,
     interpolation: {
       escapeValue: false
     }
+  }, (err, t) => {
+    i18next.t('myKey'); // key in translation namespace (defined default)
+    i18next.t('bird:myKey') // key in bird namespace
   });
 
   export default i18n;
