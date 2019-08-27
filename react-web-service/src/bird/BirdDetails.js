c7692b213ffaa9942acd9ab63ed71d1faec2aa61
Lukas J. Wensby <wensby@gmail.com>
2019-08-27 23:26:10 +0900

    Refactor BirdDetails React component

--- react-web-service/src/bird/BirdDetails.js
+++ react-web-service/src/bird/BirdDetails.js
@@ -15,28 +15,42 @@ export default function BirdDetails(props) {
     fetchData();
   }, []);
 
+  const renderCoverNameCard = () => {
+    return (
+      <div className='w-100 d-flex justify-content-center'>
+        <div className='shadow bg-white text-center pt-1 mb-0 px-2 rounded-top'>
+          <h1 className='text-dark bird-page-name pb-2 mb-0'>
+            { t(`bird:${data.binomialName}`) }</h1>
+          <p className='font-italic font-weight-light text-muted mb-0 pb-2'>
+            { data.binomialName }</p>
+        </div>
+      </div>
+    );
+  }
+
+  const renderCover = () => {
+    return (
+      <div className='picture-cover-container rounded-top overflow-hidden'
+        style={{ backgroundImage: `url(${data.coverUrl})` }}>
+        <div className='picture-cover'></div>
+        {renderCoverNameCard()}
+      </div>
+    );
+  }
+
+  const renderPhotoCredits = () => {
+    return (
+      <div>
+        <p><small>{`Thumbnail Photo by: ${ data.thumbnailCredit }`}</small></p>
+      </div>
+    );
+  }
+
   if (data) {
     return (
-      <div className='container'>
-        <div className='row picture-cover-container rounded-top overflow-hidden'
-          style={{ backgroundImage: `url(${data.coverUrl})` }}>
-          <div className='col picture-cover'></div>
-          <div className='w-100 d-flex justify-content-center'>
-            <div className='shadow bg-white text-center pt-1 mb-0 px-2 rounded-top'>
-              <h1 className='text-dark bird-page-name pb-2 mb-0'>
-                { t(`bird:${data.binomialName}`) }</h1>
-              <p className='font-italic font-weight-light text-muted mb-0 pb-2'>
-                { data.binomialName }</p>
-            </div>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col'>
-            <p>
-              <small>{`Thumbnail Photo by: ${ data.thumbnailCredit }`}</small>
-            </p>
-          </div>
-        </div>
+      <div>
+        {renderCover()}
+        {renderPhotoCredits()}
       </div>
     );
   }
