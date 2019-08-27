76f7dddc108ed85ecb71a462029ae975613b5740
Lukas J. Wensby <wensby@gmail.com>
2019-08-27 18:56:33 +0900

    Fetch bird data from BirdService in React frontend
    This service method is not yet hooked up to the API.

--- react-web-service/src/bird/BirdDetails.js
+++ react-web-service/src/bird/BirdDetails.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import BirdService from './BirdService';
 
 export default function BirdDetails(props) {
-  return props.match.params.binomialName;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const service = new BirdService();
+      const data = await service.getBird(props.match.params.binomialName);
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
+  if (data) {
+    return (
+      <div className='container'>
+        <div className='row picture-cover-container rounded-top overflow-hidden'
+          style={{ backgroundImage: data.coverPictureUrl }}>
+          <div className='col picture-cover'></div>
+          <div className='w-100 d-flex justify-content-center'>
+            <div className='shadow bg-white text-center pt-1 mb-0 px-2 rounded-top'>
+              <h1 className='text-dark bird-page-name pb-2 mb-0'>
+                { data.name }</h1>
+              <p className='font-italic font-weight-light text-muted mb-0 pb-2'>
+                { data.binomialName }</p>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <p>
+              <small>{`Thumbnail Photo by: ${ data.thumbnailCredit }`}</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  else {
+    return null;
+  }
 }
--- react-web-service/src/bird/BirdService.js
+++ react-web-service/src/bird/BirdService.js
@@ -4,4 +4,13 @@ export default class BirdService {
     const response = await fetch(`${window._env_.API_URL}/v2/bird?q=${query}`);
     return await response.json();
   }
+
+  async getBird(binomialName) {
+    return {
+      name: binomialName,
+      binomialName: binomialName,
+      coverPictureUrl: binomialName,
+      thumbnailCredit: binomialName,
+    };
+  }
 }
