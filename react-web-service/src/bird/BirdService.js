d28646411cc438e19f3ddcb0428bb506e109dabc
Lukas J. Wensby <wensby@gmail.com>
2019-08-27 23:11:41 +0900

    Setup BirdService API URL in constructor

--- react-web-service/src/bird/BirdService.js
+++ react-web-service/src/bird/BirdService.js
@@ -1,12 +1,16 @@
 export default class BirdService {
 
+  constructor() {
+    this.apiUrl = window._env_.API_URL;
+  }
+
   async queryBirds(query) {
-    const response = await fetch(`${window._env_.API_URL}/v2/bird?q=${query}`);
+    const response = await fetch(`${this.apiUrl}/v2/bird?q=${query}`);
     return await response.json();
   }
 
   async getBird(binomialName) {
-    const url = `${window._env_.API_URL}/v2/bird/${binomialName}`;
+    const url = `${this.apiUrl}/v2/bird/${binomialName}`;
     const response = await fetch(url);
     return await response.json();
   }
