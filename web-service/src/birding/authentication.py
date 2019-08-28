ee67a7115909feac94ce1bed6803236fd4427c8b
Lukas J. Wensby <wensby@gmail.com>
2019-08-28 13:27:13 +0900

    Reformat authentication Python module

--- web-service/src/birding/authentication.py
+++ web-service/src/birding/authentication.py
@@ -1,4 +1,3 @@
-import datetime
 import os
 from base64 import b64encode
 from datetime import timedelta
@@ -8,6 +7,7 @@ import jwt
 from .mail import EmailAddress
 from .account import Username, AccountFactory
 from .account import Password
+
 
 class Authenticator:
 
@@ -28,9 +28,11 @@ class Authenticator:
     if account and self.is_account_password_correct(account, password):
       return account
 
+
 class AccountRegistrationController:
 
-  def __init__(self, account_factory: AccountFactory, account_repository, mail_dispatcher, link_factory, person_repository):
+  def __init__(self, account_factory: AccountFactory, account_repository,
+        mail_dispatcher, link_factory, person_repository):
     self.account_factory = account_factory
     self.account_repository = account_repository
     self.mail_dispatcher = mail_dispatcher
@@ -49,19 +51,23 @@ class AccountRegistrationController:
 
   def __send_registration_email(self, email_address, registration, locale):
     token = registration.token
-    link = self.link_factory.create_endpoint_external_link('authentication.get_register_form', token=token)
+    link = self.link_factory.create_endpoint_external_link(
+      'authentication.get_register_form', token=token)
     message = self.create_registration_mail_message(link, locale)
-    self.mail_dispatcher.dispatch(email_address, 'Birding Registration', message)
+    self.mail_dispatcher.dispatch(email_address, 'Birding Registration',
+                                  message)
 
   def create_registration_mail_message(self, link, locale):
     message = (
-        'Hi there, thanks for showing interest in birding. '
-        'Here is your link to the registration form: ')
+      'Hi there, thanks for showing interest in birding. '
+      'Here is your link to the registration form: ')
     return locale.text(message) + link
 
-  def perform_registration(self, raw_email, registration_token, raw_username, raw_password):
+  def perform_registration(self, raw_email, registration_token, raw_username,
+        raw_password):
     email = EmailAddress(raw_email)
-    registration = self.__find_associated_registration(email, registration_token)
+    registration = self.__find_associated_registration(email,
+                                                       registration_token)
     if not registration:
       return 'associated registration missing'
     username = Username(raw_username)
@@ -83,9 +89,11 @@ class AccountRegistrationController:
     person = self.person_repository.add_person(account.username)
     self.account_repository.set_user_account_person(account, person)
 
+
 class PasswordResetController:
 
-  def __init__(self, account_repository, password_repository, link_factory, mail_dispatcher):
+  def __init__(self, account_repository, password_repository, link_factory,
+        mail_dispatcher):
     self.account_repository = account_repository
     self.password_repository = password_repository
     self.link_factory = link_factory
@@ -96,20 +104,23 @@ class PasswordResetController:
     account = self.account_repository.find_account_by_email(email)
     if not account:
       return False
-    password_reset_token = self.password_repository.create_password_reset_token(account)
+    password_reset_token = self.password_repository.create_password_reset_token(
+      account)
     token = password_reset_token.token
     if is_rest:
-      link = self.link_factory.create_frontend_link(f'/authentication/password-reset/{token}')
+      link = self.link_factory.create_frontend_link(
+        f'/authentication/password-reset/{token}')
     else:
-      link = self.link_factory.create_endpoint_external_link('authentication.get_password_reset_form', token=token)
+      link = self.link_factory.create_endpoint_external_link(
+        'authentication.get_password_reset_form', token=token)
     message = self.__create_mail_message(link, locale)
     self.mail_dispatcher.dispatch(email, 'Birding Password Reset', message)
     return True
 
   def __create_mail_message(self, link, locale):
     message = (
-        'You have requested a password reset of your Birding account. '
-        'Please follow this link to get to your password reset form: ')
+      'You have requested a password reset of your Birding account. '
+      'Please follow this link to get to your password reset form: ')
     return locale.text(message) + link
 
   def perform_password_reset(self, token, password):
@@ -119,10 +130,12 @@ class PasswordResetController:
       self.password_repository.remove_password_reset_token(token)
       return 'success'
 
+
 class SaltFactory:
 
   def create_salt(self):
     return b64encode(os.urandom(16)).decode('utf-8')
+
 
 class AuthenticationTokenFactory:
 
@@ -132,7 +145,7 @@ class AuthenticationTokenFactory:
 
   def create_authentication_token(self,
         account_id: int,
-        expiration: timedelta=timedelta(days=0, seconds=5)
+        expiration: timedelta = timedelta(days=0, seconds=5)
   ) -> str:
     payload = {
       'exp': self.utc_now_supplier() + expiration,
@@ -141,12 +154,14 @@ class AuthenticationTokenFactory:
     }
     return jwt.encode(payload, self.secret, algorithm='HS256').decode('utf-8')
 
+
 class DecodeResult:
 
-  def __init__(self, payload: dict, error: str=None):
+  def __init__(self, payload: dict, error: str = None):
     self.ok = not error
     self.error = error
     self.payload = payload
+
 
 class AuthenticationTokenDecoder:
 
