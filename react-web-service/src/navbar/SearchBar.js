6e1aa9e9c3532f5bfca17329ce44e2a5da89ed85
Lukas J. Wensby <wensby@gmail.com>
2019-09-03 14:28:27 +0900

    Convert SearchBar to a functional component

--- react-web-service/src/navbar/SearchBar.js
+++ react-web-service/src/navbar/SearchBar.js
@@ -1,62 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { withTranslation } from "react-i18next";
-import { Redirect, withRouter } from 'react-router-dom';
+import { useReactRouter } from '../reactRouterHook';
+import { useTranslation } from 'react-i18next';
 
-class SearchBar extends Component {
+export default () => {
+  const { history } = useReactRouter();
+  const [query, setQuery] = useState('');
+  const { t } = useTranslation();
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-      redirect: false,
-    }
-  }
-
-  renderTextInput = () => {
-    const { t } = this.props;
+  const renderTextInput = () => {
     return (
       <Form.Control variant='light' aria-describedby='button-addon'
         name='query' placeholder={t('Search bird')} aria-label='Search bird'
-        onChange={event => this.setState({query: event.target.value})}
-        value={this.state.query}/>
+        onChange={event => setQuery(event.target.value)}
+        value={query}/>
     );
   };
 
-  renderButton = () => {
-    const { t } = this.props;
+  const renderButton = () => {
     return (
       <Button variant="light" className="rounded-0" type="submit"
         id="button-addon">{t('Search')}</Button>
     );
   };
 
-  onFormSubmit = event => {
-    const { query } = this.state;
+  const onFormSubmit = event => {
     event.preventDefault();
-    this.props.history.push(`/bird/search?q=${query}`);
-    this.setState({query: ''});
+    history.push(`/bird/search?q=${query}`);
+    setQuery('');
   }
 
-  render() {
-    const { query, redirect } = this.state;
-
-    if (redirect) {
-        return <Redirect to={`/bird?q=${query}`} />
-    }
-    return (
-      <form id="birdSearchForm" onSubmit={this.onFormSubmit}>
-        <InputGroup size="lg">
-          {this.renderTextInput()}
-          <InputGroup.Append>
-            {this.renderButton()}
-          </InputGroup.Append>
-        </InputGroup>
-      </form>
-    );
-  }
+  return (
+    <form id="birdSearchForm" onSubmit={onFormSubmit}>
+      <InputGroup size="lg">
+        {renderTextInput()}
+        <InputGroup.Append>
+          {renderButton()}
+        </InputGroup.Append>
+      </InputGroup>
+    </form>
+  );
 }
-
-export default withRouter(withTranslation()(SearchBar));
