3e69b9855265d097be66a4cb724970222f5a19cc
Lukas J. Wensby <wensby@gmail.com>
2019-09-03 11:46:19 +0900

    Extract Main component from Page

---
+++ react-web-service/src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from "react-router-dom";
+import Settings from './settings/settings'
+import Home from './home/Home.js';
+import Authentication from './authentication/Authentication.js'
+import Bird from './bird/Bird.js';
+import Sighting from './sighting/Sighting.js';
+
+export default () => {
+  return (
+    <main role="main">
+      <Route path='/' exact component={Home} />
+      <Route path='/authentication' component={Authentication} />
+      <Route path='/settings/' exact component={Settings} />
+      <Route path='/bird' component={Bird} />
+      <Route path='/sighting' component={Sighting} />
+    </main>
+  );
+}
--- react-web-service/src/Page.js
+++ react-web-service/src/Page.js
@@ -2,15 +2,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import React from 'react';
-import { Route } from "react-router-dom";
 import Navbar from './navbar/navbar.js';
 import SideMenu from './sidemenu.js';
-import Settings from './settings/settings'
-import Home from './home/Home.js';
 import './App.css';
-import Authentication from './authentication/Authentication.js'
-import Bird from './bird/Bird.js';
-import Sighting from './sighting/Sighting.js';
+import Main from './Main.js';
 
 export default () => {
   return (
@@ -18,13 +13,7 @@ export default () => {
       <Navbar />
       <div className='main-grid navbar-pushed'>
         <SideMenu />
-        <main role="main">
-          <Route path='/' exact component={Home} />
-          <Route path='/authentication' component={Authentication} />
-          <Route path='/settings/' exact component={Settings} />
-          <Route path='/bird' component={Bird} />
-          <Route path='/sighting' component={Sighting} />
-        </main>
+        <Main />
       </div>
     </>
   );
