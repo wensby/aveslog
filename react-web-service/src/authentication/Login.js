b780a85d3cd5908c03ffc63c720dd1af0ab34b0e
Lukas J. Wensby <wensby@gmail.com>
2019-09-03 12:32:47 +0900

    Extract LoginForm component to own file

--- react-web-service/src/authentication/Login.js
+++ react-web-service/src/authentication/Login.js
@@ -1,70 +1,8 @@
-import React, { Component, useState, useContext } from 'react';
-import { withTranslation, useTranslation } from 'react-i18next';
+import React, { Component } from 'react';
+import { withTranslation } from 'react-i18next';
 import AuthenticationService from './AuthenticationService.js'
 import { Link } from 'react-router-dom';
-import { AuthenticationContext } from './AuthenticationContext.js';
-
-function LoginForm(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const authentication = new AuthenticationService();
-  const { t } = useTranslation();
-  const { onAuthenticated } = useContext(AuthenticationContext);
-
-  const handleLoginFormSubmit = async event => {
-    try {
-      event.preventDefault();
-      const response = await authentication.fetchAuthenticationToken(username, password);
-      if (response.status === 'success') {
-        onAuthenticated(response.authToken);
-        props.history.push("/");
-      }
-      else {
-        props.setErrorMessage('Login failed.');
-        setUsername('');
-        setPassword('');
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <form onSubmit={handleLoginFormSubmit}>
-      <div className="form-row">
-        <div className="form-group col-6">
-          <label htmlFor="usernameInput">{t('Username')}</label>
-          <input
-            value={username}
-            onChange={event => setUsername(event.target.value)}
-            id="usernameInput"
-            className="form-control"
-            type="text"
-            name="username"
-            placeholder={t('Username')} />
-        </div>
-        <div className="form-group col-6">
-          <label htmlFor="passwordInput">{t('Password')}</label>
-          <input
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-            id="passwordInput"
-            className="form-control"
-            type="password"
-            name="password"
-            placeholder={t('Password')} />
-        </div>
-      </div>
-      <div className="d-flex flex-row">
-        <Link
-          to="/authentication/register"
-          className="btn btn-secondary">
-          {t('Register new account')}</Link>
-        <button type='submit' className="btn btn-primary ml-auto">{t('Login')}</button>
-      </div>
-    </form>
-  );
-}
+import LoginForm from './LoginForm.js';
 
 class Login extends Component {
 
