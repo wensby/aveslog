f23c06fa7b503bd4d2c531f2044c38fefda0ecea
Lukas J. Wensby <wensby@gmail.com>
2019-09-03 11:42:12 +0900

    Refactor Page to a functional component

--- react-web-service/src/Page.js
+++ react-web-service/src/Page.js
@@ -1,41 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from "react-router-dom";
 import Navbar from './navbar/navbar.js';
 import SideMenu from './sidemenu.js';
 import Settings from './settings/settings'
 import Home from './home/Home.js';
 import './App.css';
-import { withTranslation } from 'react-i18next';
 import Authentication from './authentication/Authentication.js'
 import Bird from './bird/Bird.js';
 import Sighting from './sighting/Sighting.js';
 
-class Page extends Component {
-
-  constructor(props) {
-    super(props);
-  };
-
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className='main-grid navbar-pushed'>
-          <SideMenu />
-          <main role="main">
-            <Route path='/' exact component={Home} />
-            <Route path='/authentication' component={Authentication} />
-            <Route path='/settings/' exact component={Settings} />
-            <Route path='/bird' component={Bird} />
-            <Route path='/sighting' component={Sighting} />
-          </main>
-        </div>
-      </>
-    );
-  }
+export default () => {
+  return (
+    <>
+      <Navbar />
+      <div className='main-grid navbar-pushed'>
+        <SideMenu />
+        <main role="main">
+          <Route path='/' exact component={Home} />
+          <Route path='/authentication' component={Authentication} />
+          <Route path='/settings/' exact component={Settings} />
+          <Route path='/bird' component={Bird} />
+          <Route path='/sighting' component={Sighting} />
+        </main>
+      </div>
+    </>
+  );
 }
-
-export default withTranslation()(Page);
