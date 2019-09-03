214aa40942b58f35e1291c3548cbc0dc9232d448
Lukas J. Wensby <wensby@gmail.com>
2019-09-03 15:23:11 +0900

    Rename Register to RegisterEmail

--- react-web-service/src/authentication/Authentication.js
+++ react-web-service/src/authentication/Authentication.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import PasswordReset from './PasswordReset.js';
 import Login from './Login.js';
-import Register from './Register.js';
+import RegisterEmail from './RegisterEmail.js';
 
 export default ({ match }) => {
   return (
     <Switch>
       <Route path={`${match.path}/login`} component={Login}/>
-      <Route path={`${match.path}/register`} component={Register}/>
+      <Route path={`${match.path}/register`} component={RegisterEmail}/>
       <Route path={`${match.path}/password-reset`} component={PasswordReset}/>
     </Switch>
   );
