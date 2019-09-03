22111cf6e15ada1bc230108798b869e4f111a48e
Lukas J. Wensby <wensby@gmail.com>
2019-09-03 15:21:16 +0900

    Rework Register to a functional component

--- react-web-service/src/authentication/Register.js
+++ react-web-service/src/authentication/Register.js
@@ -1,54 +1,40 @@
-import React, { Component } from 'react';
-import { withTranslation } from 'react-i18next';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import AuthenticationService from './AuthenticationService.js';
 import { Link } from 'react-router-dom'
 
-class PasswordReset extends Component {
+export default () => {
+  const [email, setEmail] = useState('');
+  const [alert, setAlert] = useState(null);
+  const { t } = useTranslation();
+  const authentication = new AuthenticationService();
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      alert: null,
-    }
-    this.authentication = new AuthenticationService();
-  }
-
-  renderAlert = () => {
-    const { t } = this.props;
-    const { alert } = this.state;
+  const renderAlert = () => {
     if (alert) {
-      return (
-        <div className={ `col alert alert-${alert.type}` }
-          role="alert">{ t(alert.message) }</div>
-      );
+      return <div className={`col alert alert-${alert.type}`}
+        role='alert'>{t(alert.message)}</div>;
     }
     else {
       return null;
     }
   }
 
-  handleFormSubmit = async event => {
+  const handleFormSubmit = async event => {
     try {
       event.preventDefault();
-      const { email } = this.state;
-      const response = await this.authentication.postRegistrationEmail(email);
+      const response = await authentication.postRegistrationEmail(email);
       if (response.status === 'success') {
-        this.setState({
-          alert: {
-            type: 'success',
-            message: 'registration-email-submit-success-message',
-          }
+        setAlert({
+          type: 'success',
+          message: 'registration-email-submit-success-message',
         });
       }
       else {
-        this.setState({
-          alert: {
-            type: 'danger',
-            message: 'registration-email-submit-failure-message',
-          },
-          email: '',
+        setAlert({
+          type: 'danger',
+          message: 'registration-email-submit-failure-message',
         });
+        setEmail('');
       }
     }
     catch (e) {
@@ -56,55 +42,43 @@ class PasswordReset extends Component {
     }
   }
 
-  onEmailInputChanged = event => {
-    this.setState({
-      email: event.target.value
-    });
-  }
-
-  render() {
-    const { t } = this.props;
-    const { email } = this.state;
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1>{ t('Registration') }</h1>
-            <p>{ t('register-link-request-prompt-message') }</p>
-          </div>
-        </div>
-        {this.renderAlert()}
-        <div className="row">
-          <div className="col">
-            <form onSubmit={this.handleFormSubmit}>
-              <div className="form-group">
-                <label htmlFor="emailInput">{ t('Email address') }</label>
-                <input
-                  value={email}
-                  onChange={this.onEmailInputChanged}
-                  id="emailInput"
-                  className="form-control"
-                  type="text"
-                  name="email"
-                  placeholder={ t('Enter email') }
-                  />
-              </div>
-              <button
-                className="btn btn-primary"
-                type="submit">
-                { t('Continue') }
-                </button>
-            </form>
-          </div>
-        </div>
-        <div className="row mt-2">
-          <div className="col">
-            <Link to='/authentication/login'>{ t('Back to login') }</Link>
-          </div>
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <h1>{t('Registration')}</h1>
+          <p>{t('register-link-request-prompt-message')}</p>
         </div>
       </div>
-    );
-  }
+      {renderAlert()}
+      <div className='row'>
+        <div className='col'>
+          <form onSubmit={handleFormSubmit}>
+            <div className='form-group'>
+              <label htmlFor='emailInput'>{t('Email address')}</label>
+              <input
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+                id='emailInput'
+                className='form-control'
+                type='text'
+                name='email'
+                placeholder={t('Enter email')}
+              />
+            </div>
+            <button
+              className='btn btn-primary'
+              type='submit'>
+              {t('Continue')}
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className='row mt-2'>
+        <div className='col'>
+          <Link to='/authentication/login'>{t('Back to login')}</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default withTranslation()(PasswordReset);
