18b32afd372d577f842acc89e419843172dc0dbd
Lukas J. Wensby <wensby@gmail.com>
2019-09-08 16:25:00 +0900

    Reformat frontend RegistrationForm component

--- react-web-service/src/authentication/RegistrationForm.js
+++ react-web-service/src/authentication/RegistrationForm.js
@@ -35,7 +35,9 @@ export default () => {
       <div className='row'>
         <div className='col'>
           <h2>{ t('Register') }</h2>
-          <p>{ t('Fill out the following form to complete your registration.') }</p>
+          <p>
+            { t('Fill out the following form to complete your registration.') }
+          </p>
         </div>
       </div>
       {renderAlert()}
@@ -44,11 +46,26 @@ export default () => {
           <form onSubmit={handleFormSubmit}>
             <div className='form-group'>
               <label htmlFor='emailInput'>{ t('Email address') }</label>
-              <input className='form-control' id='emailInput' type='text' name='email' readonly placeholder={ email } value={ email }/>
+              <input
+                className='form-control'
+                id='emailInput'
+                type='text'
+                name='email'
+                readonly
+                placeholder={ email }
+                value={ email }/>
             </div>
             <div className='form-group'>
               <label htmlFor='usernameInput'>{ t('Username') }</label>
-              <input id='usernameInput' className='form-control' type='username' name='username' aria-describedby='usernameHelpBlock' placeholder={ t('Username')} required pattern='[A-Za-z0-9._-]{5,32}'/>
+              <input
+                id='usernameInput'
+                className='form-control'
+                type='username'
+                name='username'
+                aria-describedby='usernameHelpBlock'
+                placeholder={ t('Username')}
+                required
+                pattern='[A-Za-z0-9._-]{5,32}'/>
               <small id='usernameHelpBlock' className='form-text text-muted'>
                 { t('Must be 5 to 32 characters long, contain only letters, numbers, dashes (-), periods (.), and underscores (_).') }
               </small>
@@ -58,7 +75,15 @@ export default () => {
             </div>
             <div className='form-group'>
               <label htmlFor='passwordInput'>{ t('Password') }</label>
-              <input id='passwordInput' className='form-control' type='password' name='password' aria-describedby='passwordHelpBlock' placeholder={ t('Password')} required pattern='.{8,128}'/>
+              <input
+                id='passwordInput'
+                className='form-control'
+                type='password'
+                name='password'
+                aria-describedby='passwordHelpBlock'
+                placeholder={ t('Password')}
+                required
+                pattern='.{8,128}'/>
               <small id='passwordHelpBlock' className='form-text text-muted'>
                 { t('Must be 8 to 128 character long.') }
               </small>
@@ -70,8 +95,15 @@ export default () => {
               </div>
             </div>
             <div className='form-group'>
-              <label htmlFor='confirmPasswordInput'>{ t('Confirm password') }</label>
-              <input id='confirmPasswordInput' className='form-control' type='password' name='confirmPassword' placeholder={ t('Confirm password')}/>
+              <label htmlFor='confirmPasswordInput'>
+                { t('Confirm password') }
+              </label>
+              <input
+                id='confirmPasswordInput'
+                className='form-control'
+                type='password'
+                name='confirmPassword'
+                placeholder={ t('Confirm password')}/>
               <div className='valid-feedback'>
                 { t('Both passwords matches!') }
               </div>
@@ -81,7 +113,12 @@ export default () => {
             </div>
             <div className='form-group'>
               <div className='form-check'>
-                <input className='form-check-input' type='checkbox' value='' id='tocCheckbox' required/>
+                <input
+                  className='form-check-input'
+                  type='checkbox'
+                  value=''
+                  id='tocCheckbox'
+                  required/>
                 <label className='form-check-label' htmlFor='tocCheckbox'>
                   { t('Agree to terms and conditions') }
                 </label>
@@ -91,7 +128,9 @@ export default () => {
               </div>
             </div>
             <input type='hidden' name='token' value={token}/>
-            <button class='btn btn-primary' type='submit'>{ t('Register') }</button>
+            <button class='btn btn-primary' type='submit'>
+              { t('Register') }
+            </button>
           </form>
         </div>
       </div>
