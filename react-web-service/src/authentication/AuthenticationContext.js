4a3ff1c734f7c229ad79724d0356eec37917d850
Lukas J. Wensby <wensby@gmail.com>
2019-09-13 15:11:31 +0900

    Recover authentication token with local storage
    The user should stay logged in even if they refresh the page or opens the page
    in another tab. In order to achieve this, the token is now also stored in
    Local Storage where it is retrieved in the AuthenticationContext.

--- react-web-service/src/authentication/AuthenticationContext.js
+++ react-web-service/src/authentication/AuthenticationContext.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AccountService from '../account/AccountService.js';
 
 const accountService = new AccountService()
@@ -8,9 +8,29 @@ function AuthenticationProvider(props) {
   const [authenticated, setAuthenticated] = useState(false);
   const [account, setAccount] = useState(null);
   const [token, setToken] = useState(null);
+
+  const resolveLocalStorageToken = async () => {
+    const localStorageToken = localStorage.getItem('authenticationToken');
+    if (authenticated && !localStorageToken) {
+      setToken(null);
+      setAuthenticated(false);
+      setAccount(false);
+    }
+    else if (!authenticated && localStorageToken) {
+      setToken(localStorageToken);
+      setAuthenticated(true);
+      const account = await accountService.fetchAccount(localStorageToken);
+      setAccount(account);
+    }
+  }
+
+  useEffect(() => {
+    resolveLocalStorageToken();
+  });
   
   const onAuthenticated = async token => {
     setToken(token);
+    localStorage.setItem('authenticationToken', token);
     setAuthenticated(true);
     const account = await accountService.fetchAccount(token);
     setAccount(account);
@@ -20,6 +40,7 @@ function AuthenticationProvider(props) {
     setToken(null);
     setAuthenticated(false);
     setAccount(false);
+    localStorage.removeItem('authenticationToken');
   }
 
   return (
