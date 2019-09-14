f0158cb7a5a3e01026fe41de929a369b00ba0d3f
Lukas J. Wensby <wensby@gmail.com>
2019-09-14 13:54:33 +0900

    Refactor test_authentication_rest_api.py

--- web-service/src/tests/test_authentication_rest_api.py
+++ web-service/src/tests/test_authentication_rest_api.py
@@ -1,4 +1,3 @@
-import json
 from http import HTTPStatus
 
 from flask import Response
@@ -8,49 +7,38 @@ from test_util import AppTestCase
 
 class TestLogin(AppTestCase):
 
-  def test_get_token_when_correct_credentials(self):
-    self.db_insert_account(4, 'myUsername', 'my@email.com', None, None)
-    self.db_insert_password(4, 'myPassword')
+  def test_get_token_when_ok(self):
+    self.db_insert_account(1, 'myUsername', 'my@email.com', None, None)
+    self.db_insert_password(1, 'myPassword')
 
     response = self.get_authentication_token('myUsername', 'myPassword')
 
     self.assertEqual(response.status_code, HTTPStatus.OK)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'success')
-    self.assertEqual(data['message'], 'Successfully logged in.')
-    self.assertIn('authToken', data)
+    self.assertEqual(response.json['status'], 'success')
+    self.assertEqual(response.json['message'], 'Successfully logged in.')
+    self.assertIn('authToken', response.json)
 
   def test_get_token_when_incorrect_credentials(self):
-    self.db_insert_account(4, 'myUsername', 'my@email.com', None, None)
-    self.db_insert_password(4, 'myPassword')
+    self.db_insert_account(1, 'myUsername', 'my@email.com', None, None)
+    self.db_insert_password(1, 'myPassword')
 
     response = self.get_authentication_token('somethingElse', 'notGood')
 
     self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'failure')
-    self.assertEqual(data['message'], 'Try again')
-    self.assertNotIn('auth_token', data)
+    self.assertEqual(response.json, {
+      'status': 'failure',
+      'message': 'Try again',
+    })
 
-  def get_authentication_token(self, username, password):
-    return self.client.get(
-      f'/v2/authentication/token?username={username}&password={password}'
-    )
+  def get_authentication_token(self, username: str, password: str) -> Response:
+    resource = '/v2/authentication/token'
+    query = f'username={username}&password={password}'
+    return self.client.get(f'{resource}?{query}')
 
 
 class TestPasswordReset(AppTestCase):
 
-  def test_post_password_reset_email_when_email_not_linked_with_account(self):
-    self.db_insert_locale(1, 'en')
-
-    response = self.post_password_reset_email('hulot@mail.com')
-
-    self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'failure')
-    self.assertEqual(data['message'], 'E-mail not associated with any account')
-
-  def test_post_password_reset_email_when_email_associated_with_account(self):
+  def test_post_password_reset_email_when_ok(self):
     self.db_insert_person(1)
     self.db_insert_account(1, 'bolas', 'bolas@mail.com', 1, None)
     self.db_insert_locale(1, 'en')
@@ -58,9 +46,21 @@ class TestPasswordReset(AppTestCase):
     response = self.post_password_reset_email('bolas@mail.com')
 
     self.assertEqual(response.status_code, HTTPStatus.OK)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'success')
-    self.assertEqual(data['message'], 'Password reset link sent to e-mail')
+    self.assertEqual(response.json, {
+      'status': 'success',
+      'message': 'Password reset link sent to e-mail',
+    })
+
+  def test_post_password_reset_email_when_email_not_linked_with_account(self):
+    self.db_insert_locale(1, 'en')
+
+    response = self.post_password_reset_email('hulot@mail.com')
+
+    self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
+    self.assertEqual(response.json, {
+      'status': 'failure',
+      'message': 'E-mail not associated with any account',
+    })
 
   def test_post_password_reset_when_password_reset_not_first_created(self):
     response = self.post_password_reset('myToken', 'myNewPassword')
@@ -84,27 +84,26 @@ class TestPasswordReset(AppTestCase):
       'message': 'Password reset successfully',
     })
 
-  def post_password_reset_email(self, email):
+  def post_password_reset_email(self, email: str) -> Response:
     json = {'email': email}
     return self.client.post('/v2/authentication/password-reset', json=json)
 
-  def post_password_reset(self, token, password) -> Response:
-    return self.client.post(
-      f'/v2/authentication/password-reset/{token}',
-      json={'password': password}
-    )
+  def post_password_reset(self, token: str, password: str) -> Response:
+    resource = f'/v2/authentication/password-reset/{token}'
+    return self.client.post(resource, json={'password': password})
 
 
 class TestRegistration(AppTestCase):
 
-  def test_post_registration_email_when_email_not_taken(self):
+  def test_post_registration_email_when_ok(self):
     self.db_insert_locale(1, 'en')
 
     response = self.post_registration_email('hulot@mail.com')
 
     self.assertEqual(response.status_code, HTTPStatus.OK)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'success')
+    self.assertEqual(response.json, {
+      'status': 'success',
+    })
 
   def test_post_registration_email_when_email_invalid(self):
     self.db_insert_locale(1, 'en')
@@ -112,9 +111,10 @@ class TestRegistration(AppTestCase):
     response = self.post_registration_email('hulot')
 
     self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'failure')
-    self.assertEqual(data['message'], 'Email invalid')
+    self.assertEqual(response.json, {
+      'status': 'failure',
+      'message': 'Email invalid',
+    })
 
   def test_post_registration_email_when_email_taken(self):
     self.db_insert_locale(1, 'en')
@@ -124,22 +124,25 @@ class TestRegistration(AppTestCase):
     response = self.post_registration_email('hulot@mail.com')
 
     self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'failure')
-    self.assertEqual(data['message'], 'Email taken')
+    self.assertEqual(response.json, {
+      'status': 'failure',
+      'message': 'Email taken',
+    })
 
-  def test_get_registration_when_token_present(self):
-    self.db_insert_registration('hulot@mail.com', 'myToken')
-
-  def test_get_registration_when_present(self):
+  def test_get_registration_when_ok(self):
     self.db_insert_registration('hulot@mail.com', 'myToken')
 
     response = self.get_registration('myToken')
 
     self.assertEqual(response.status_code, HTTPStatus.OK)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'success')
-    self.assertEqual(data['result']['registration']['email'], 'hulot@mail.com')
+    self.assertEqual(response.json, {
+      'status': 'success',
+      'result': {
+        'registration': {
+          'email': 'hulot@mail.com',
+        },
+      },
+    })
 
   def test_get_registration_when_missing(self):
     response = self.get_registration('myToken')
@@ -152,9 +155,10 @@ class TestRegistration(AppTestCase):
     response = self.post_registration('myToken', 'myUsername', 'myPassword')
 
     self.assertEqual(response.status_code, HTTPStatus.OK)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'success')
-    self.assertEqual(data['message'], 'Registration successful')
+    self.assertEqual(response.json, {
+      'status': 'success',
+      'message': 'Registration successful',
+    })
 
   def test_post_registration_when_username_already_taken(self):
     self.db_insert_person(1)
@@ -164,24 +168,22 @@ class TestRegistration(AppTestCase):
     response = self.post_registration('myToken', 'takenUsername', 'password')
 
     self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
-    data = json.loads(response.data.decode('utf-8'))
-    self.assertEqual(data['status'], 'failure')
-    self.assertEqual(data['message'], 'Username already taken')
+    self.assertEqual(response.json, {
+      'status': 'failure',
+      'message': 'Username already taken',
+    })
 
-  def post_registration_email(self, email):
-    return self.client.post(
-      '/v2/authentication/registration',
-      json={'email': email})
+  def post_registration_email(self, email: str) -> Response:
+    resource = '/v2/authentication/registration'
+    return self.client.post(resource, json={'email': email})
 
   def get_registration(self, token: str) -> Response:
     return self.client.get(f'/v2/authentication/registration/{token}')
 
-  def post_registration(
-        self,
+  def post_registration(self,
         token: str,
         username: str,
-        password: str
-  )-> Response:
-    return self.client.post(
-      f'/v2/authentication/registration/{token}',
-      json={'username': username, 'password': password})
+        password: str) -> Response:
+    resource = f'/v2/authentication/registration/{token}'
+    json = {'username': username, 'password': password}
+    return self.client.post(resource, json=json)
