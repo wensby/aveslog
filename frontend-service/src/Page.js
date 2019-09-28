16c50aade3c2a9ae4162b43ef01c2dc6592de824
Lukas J. Wensby <wensby@gmail.com>
2019-09-28 13:59:08 +0900

    Correctly stick footer at bottom of page
    There was a problem when the content of the page was not enough to force the
    footer down to its correct size at the bottom of the page, meaning that the
    footer seemingly covered a vast amount of space on the page. With this commit,
    this has been corrected, and the footer now has a fixed height and always appear
    at the very bottom of the page.
    
    References:
    1. Fraser, D. "How to keep your footer where it belongs ?". freeCodeCamp.
       [Online] Available from:
       https://www.freecodecamp.org/news/how-to-keep-your-footer-where-it-belongs-59c6aa05c59c/
       [Accessed 28th of September 2019]

--- frontend-service/src/Page.js
+++ frontend-service/src/Page.js
@@ -11,13 +11,13 @@ import './style.scss';
 
 export default () => {
   return (
-    <>
+    <div className='page-container'>
       <TopNavbar />
       <div className='main-grid navbar-pushed'>
         <SideMenu />
         <Main />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
