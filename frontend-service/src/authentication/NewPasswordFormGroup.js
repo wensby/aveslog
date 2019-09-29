ce40585d3440c6e7c1db930f5f128c600a91f876
Lukas J. Wensby <wensby@gmail.com>
2019-09-29 15:24:51 +0900

    Extract NewPasswordFormGroup component
    This commit returns the valid password if it has been inputted correctly,
    otherwise just returns the empty string.

---
+++ frontend-service/src/authentication/NewPasswordFormGroup.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export default ({ onChange, showFeedback }) => {
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [validPattern, setValidPattern] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
+
+  useEffect(() => {
+    setValidPattern(/^.{8,128}$/.test(password));
+    setPasswordsMatch(password == passwordConfirmation);
+  }, [password, passwordConfirmation]);
+
+  useEffect(() => {
+    const validPassword = validPattern && passwordsMatch ? password : '';
+    onChange(validPassword);
+  }, [validPattern, passwordsMatch])
+
+  return <>
+    <PasswordFormGroup value={password} onChange={setPassword}
+      valid={validPattern} showFeedback={showFeedback} />
+    <PasswordConfirmationFormGroup value={passwordConfirmation}
+      onChange={setPasswordConfirmation}
+      valid={passwordsMatch} showFeedback={showFeedback} />
+  </>;
+}
+
+const PasswordFormGroup = ({ value, onChange, valid, showFeedback }) => {
+  const { t } = useTranslation();
+  const validClassName = valid ? ' is-valid' : ' is-invalid';
+  const feedbackClass = showFeedback ? validClassName : '';
+
+  return (
+    <div className='form-group'>
+      <label htmlFor='passwordInput'>{t('Password')}</label>
+      <input id='passwordInput' className={`form-control ${feedbackClass}`}
+        type='password' name='password' aria-describedby='passwordHelpBlock'
+        placeholder={t('Password')} value={value}
+        onChange={event => onChange(event.target.value)} />
+      <small id='passwordHelpBlock' className='form-text text-muted'>
+        {t('password-format-help-message')}
+      </small>
+      <div className='valid-feedback'>{t('password-valid-feedback')}</div>
+      <div className='invalid-feedback'>{t('password-invalid-feedback')}</div>
+    </div>
+  );
+};
+
+const PasswordConfirmationFormGroup = props => {
+  const { value, onChange, valid, showFeedback } = props;
+  const { t } = useTranslation();
+  const validClassName = valid && value ? ' is-valid' : ' is-invalid';
+  const feedbackClass = showFeedback ? validClassName : '';
+
+  return (
+    <div className='form-group'>
+      <label htmlFor='confirmPasswordInput'>
+        {t('password-confirm-password-label')}
+      </label>
+      <input id='confirmPasswordInput' type='password' name='confirmPassword'
+        className={`form-control ${feedbackClass}`} value={value}
+        placeholder={t('password-confirm-password-label')}
+        onChange={event => onChange(event.target.value)} />
+      <div className='valid-feedback'>
+        {t('password-confirmation-valid-feedback')}
+      </div>
+      <div className='invalid-feedback'>
+        {t('password-confirmation-invalid-feedback')}
+      </div>
+    </div>
+  );
+};
--- frontend-service/src/authentication/registration/CredentialsForm.js
+++ frontend-service/src/authentication/registration/CredentialsForm.js
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import NewPasswordFormGroup from '../NewPasswordFormGroup';
 
 
 export default ({ email, token, onSubmit, takenUsernames }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [usernameValid, setUsernameValid] = useState(false);
-  const [passwordValid, setPasswordValid] = useState(false);
-  const [passwordsMatch, setPasswordsMatch] = useState(false);
   const [showFeedback, setShowFeedback] = useState(false);
 
   useEffect(() => {
     setUsernameValid(!takenUsernames.includes(username) && /^[A-Za-z0-9._-]{5,32}$/.test(username));
-    setPasswordValid(/^.{8,128}$/.test(password));
-    setPasswordsMatch(password == passwordConfirmation);
-  }, [takenUsernames, username, password, passwordConfirmation]);
+  }, [takenUsernames, username]);
 
   const handleSubmit = event => {
     event.preventDefault();
-    if (usernameValid && passwordValid && passwordsMatch) {
+    if (usernameValid && password) {
       onSubmit([username, password]);
     }
     setShowFeedback(true);
@@ -30,11 +26,8 @@ export default ({ email, token, onSubmit, takenUsernames }) => {
       <EmailFormGroup value={email} />
       <UsernameFormGroup value={username} onChange={setUsername}
         valid={usernameValid} showFeedback={showFeedback} />
-      <PasswordFormGroup value={password} onChange={setPassword}
-        valid={passwordValid} showFeedback={showFeedback} />
-      <PasswordConfirmationFormGroup value={passwordConfirmation}
-        onChange={setPasswordConfirmation}
-        valid={passwordsMatch} showFeedback={showFeedback} />
+      <NewPasswordFormGroup onChange={setPassword}
+        showFeedback={showFeedback} />
       <input type='hidden' name='token' value={token} />
       <SubmitButton />
     </form>
@@ -68,52 +61,6 @@ const UsernameFormGroup = ({ value, onChange, valid, showFeedback }) => {
         {t('username-help-block')}
       </small>
       <div className='valid-feedback'>{t('username-valid-feedback')}</div>
-    </div>
-  );
-};
-
-const PasswordFormGroup = ({ value, onChange, valid, showFeedback }) => {
-  const { t } = useTranslation();
-  const validClassName = valid ? ' is-valid' : ' is-invalid';
-  const feedbackClass = showFeedback ? validClassName : '';
-
-  return (
-    <div className='form-group'>
-      <label htmlFor='passwordInput'>{t('Password')}</label>
-      <input id='passwordInput' className={`form-control ${feedbackClass}`}
-        type='password' name='password' aria-describedby='passwordHelpBlock'
-        placeholder={t('Password')} value={value}
-        onChange={event => onChange(event.target.value)} />
-      <small id='passwordHelpBlock' className='form-text text-muted'>
-        {t('password-format-help-message')}
-      </small>
-      <div className='valid-feedback'>{t('password-valid-feedback')}</div>
-      <div className='invalid-feedback'>{t('password-invalid-feedback')}</div>
-    </div>
-  );
-};
-
-const PasswordConfirmationFormGroup = props => {
-  const { value, onChange, valid, showFeedback } = props;
-  const { t } = useTranslation();
-  const validClassName = valid && value ? ' is-valid' : ' is-invalid';
-  const feedbackClass = showFeedback ? validClassName : '';
-
-  return (
-    <div className='form-group'>
-      <label htmlFor='confirmPasswordInput'>
-        {t('password-confirm-password-label')}
-      </label>
-      <input id='confirmPasswordInput' type='password' name='confirmPassword'
-        className={`form-control ${feedbackClass}`} value={value}
-        placeholder={t('password-confirm-password-label')}
-        onChange={event => onChange(event.target.value)} />
-      <div className='valid-feedback'>
-        {t('password-confirmation-valid-feedback')}
-      </div>
-      <div className='invalid-feedback'>
-        {t('password-confirmation-invalid-feedback')}
-      </div>
     </div>
   );
 };
