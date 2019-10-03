913dd4b2887e239e62d7df55ac0bca22cb92b37f
Lukas J. Wensby <wensby@gmail.com>
2019-10-03 14:36:55 +0900

    Set max-age on served static build files

--- frontend-service/server.js
+++ frontend-service/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build'), {
+  maxAge: 86400000,
+}));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
