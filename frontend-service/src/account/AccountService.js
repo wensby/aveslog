d3b5474678ca268ebfc79c3ab3813482650a9fe4
Lukas J. Wensby <wensby@gmail.com>
2019-10-03 23:47:40 +0900

    Add registered accounts to logged in screen

--- frontend-service/src/account/AccountService.js
+++ frontend-service/src/account/AccountService.js
@@ -1,5 +1,16 @@
 export default class AccountService {
 
+  async fetchAccounts(authToken) {
+    const url = `${window._env_.API_URL}/account`;
+    const response = await fetch(url, {
+      'headers': {
+        'authToken': authToken
+      }
+    });
+    const jsonResponse = await response.json();
+    return jsonResponse.result;
+  }
+
   async fetchAccount(authToken) {
     const url = `${window._env_.API_URL}/account/me`;
     const response = await fetch(url, {
--- frontend-service/src/home/Home.js
+++ frontend-service/src/home/Home.js
@@ -1,14 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AuthenticationContext } from '../authentication/AuthenticationContext';
+import { Link } from 'react-router-dom';
+import AccountService from '../account/AccountService';
 
 export default () => {
-  const { authenticated } = useContext(AuthenticationContext);
+  const { authenticated, token } = useContext(AuthenticationContext);
   const { t } = useTranslation();
+  const [usernames, setUsernames] = useState([]);
+  const accountService = new AccountService();
+
+  const fetchAccounts = async () => {
+    const accounts = await accountService.fetchAccounts(token);
+    setUsernames(accounts.map(a => a.username));
+  }
+
+  useEffect(() => {
+    if (authenticated) {
+      fetchAccounts();
+    }
+  }, []);
 
   if (authenticated) {
     return (
-      <h1>{t("It's birding time!")}</h1>
+      <>
+        <h1>{t("It's birding time!")}</h1>
+        {usernames.map(name => (
+          <div>
+            <Link to={`/profile/${name}`}>{name}</Link>
+          </div>
+        ))}
+      </>
     );
   }
 
