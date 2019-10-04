8091a458d957671e4833b11b98e45fb8c9bdb8ee
Lukas J. Wensby <wensby@gmail.com>
2019-10-04 11:03:06 +0900

    Only set 1 day maxAge on media static files
    Having a 1 day maxAge on all files was annoying when I made changes to the page
    that didn't show up unless you forced the browser to refetch cached files.

--- frontend-service/server.js
+++ frontend-service/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build'), {
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build', 'media'), {
   maxAge: 86400000,
 }));
 
