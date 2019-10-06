e6975635cfd6b4fd8774ba946bff7db5f736b36b
Lukas J. Wensby <wensby@gmail.com>
2019-10-06 21:55:55 +0900

    Remove unused functions

--- api-service/src/birding/time.py
+++ api-service/src/birding/time.py
@@ -1,15 +1,11 @@
 from datetime import date
 from datetime import time
 
+
 def parse_date(date_string):
   return date.fromisoformat(date_string)
+
 
 def parse_time(time_string):
   if time_string:
     return time.fromisoformat(time_string)
-
-
-def format_date_time(date, time=None):
-  if time:
-    return f'{date.isoformat()} {time.isoformat()}'
-  return date.isoformat()
--- api-service/src/tests/test_time.py
+++ api-service/src/tests/test_time.py
@@ -1,13 +1,10 @@
-from datetime import time, date
+from datetime import time
 from unittest import TestCase
 
-from birding.time import parse_time, format_date_time
+from birding.time import parse_time
 
 
 class TestTime(TestCase):
 
   def test_parse_time_parses_iso_format_correctly(self):
     self.assertEqual(parse_time('19:03'), time(19, 3))
-
-  def test_format_date_time_without_time(self):
-    self.assertEqual(format_date_time(date(2019, 7, 13)), '2019-07-13')
