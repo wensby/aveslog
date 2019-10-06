3b263345a3069ecdd3b0bbec3f8ac015d8efe2cb
Lukas J. Wensby <wensby@gmail.com>
2019-10-06 23:53:05 +0900

    Unit test Bird
    100% test coverage.

--- api-service/src/tests/test_bird.py
+++ api-service/src/tests/test_bird.py
@@ -13,9 +13,14 @@ class TestBird(TestCase):
   def test_construction(self):
     Bird(4, 'Pica pica')
 
+  def test_eq_when_same_bird(self) -> None:
+    self.assertEqual(Bird(1, 'Pica pica'), Bird(1, 'Pica pica'))
+
   def test_eq_false_when_other_type(self):
     self.assertNotEqual(Bird(4, 'Pica pica'), 'Bird(4, Pica pica)')
 
+  def test_repr(self) -> None:
+    self.assertEqual(repr(Bird(1, 'Pica pica')), 'Bird(1, Pica pica)')
 
 class TestBirdThumbnail(TestCase):
 
