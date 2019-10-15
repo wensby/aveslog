56df6e9bc5995580c3c3ba680532394d539cd036
Lukas J. Wensby <wensby@gmail.com>
2019-10-15 19:48:30 +0900

    Correct bird picture dimensions

--- frontend-service/src/bird/BirdCardPicture.js
+++ frontend-service/src/bird/BirdCardPicture.js
@@ -2,7 +2,7 @@ import React from 'react';
 import placeholder from './placeholder-bird.jpg';
 
 export default ({ bird, ...other }) => {
-  const style = { maxHeight: '150px' };
+  const style = { height: '150px', width: '150px', objectFit: 'cover' };
   const src = bird.thumbnailUrl || placeholder;
   return <img style={style} src={src} alt="Card" {...other} />;
 };
