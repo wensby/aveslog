ed8ea19f457b95ca0929a7a1cef42669f10c6385
Lukas J. Wensby <wensby@gmail.com>
2019-10-16 13:52:25 +0900

    Reorder and type hint search.py

--- api-service/src/birding/search.py
+++ api-service/src/birding/search.py
@@ -1,28 +1,38 @@
 from difflib import SequenceMatcher
+from typing import Dict, List, Optional
 
+from .bird import Bird
+from .bird import BirdRepository
 from .localization import LocaleRepository, LocaleLoader
 
 
-class BirdSearchController:
+class BirdMatch:
 
-  def __init__(self, bird_searcher):
-    self.bird_searcher = bird_searcher
+  def __init__(self, bird: Bird, query_match: float):
+    self.bird = bird
+    self.query_match = query_match
 
-  def search(self, name):
-    bird_matches = self.bird_searcher.search(name)
-    bird_matches.sort(key=lambda m: m.query_match, reverse=True)
-    return bird_matches[:100]
+
+class StringMatcher:
+
+  def match(self, a: str, b: str) -> float:
+    return SequenceMatcher(None, a.lower(), b.lower()).ratio()
+
 
 class BirdSearcher:
 
-  def __init__(self, bird_repository, locale_repository: LocaleRepository,
-        string_matcher, locale_loader: LocaleLoader):
+  def __init__(self,
+        bird_repository: BirdRepository,
+        locale_repository: LocaleRepository,
+        string_matcher: StringMatcher,
+        locale_loader: LocaleLoader,
+  ):
     self.bird_repository = bird_repository
     self.locale_repository = locale_repository
     self.string_matcher = string_matcher
     self.locale_loader = locale_loader
 
-  def search(self, name=None):
+  def search(self, name: Optional[str] = None) -> List[BirdMatch]:
     result_builder = ResultBuilder()
     birds = self.bird_repository.birds
     if name:
@@ -32,7 +42,7 @@ class BirdSearcher:
       result_builder.add_matches(language_name_matches)
     return result_builder.create_bird_matches()
 
-  def search_by_binomial_name(self, name):
+  def search_by_binomial_name(self, name) -> Dict[Bird, List[float]]:
     matches = dict()
     birds = self.bird_repository.birds
     for bird in birds:
@@ -40,15 +50,17 @@ class BirdSearcher:
         matches[bird] = [self.string_matcher.match(name, bird.binomial_name)]
     return matches
 
-  def search_by_language_names(self, name):
+  def search_by_language_names(self, name) -> Dict[Bird, List[float]]:
     matches = dict()
     birds = self.bird_repository.birds
     for dictionary in self.__get_bird_dictionaries():
       for bird in birds:
         binomial_name = bird.binomial_name
-        if binomial_name in dictionary and name.lower() in dictionary[binomial_name].lower():
+        if binomial_name in dictionary and name.lower() in dictionary[
+          binomial_name].lower():
           match = self.string_matcher.match(name, dictionary[binomial_name])
-          matches[bird] = matches[bird] + [match] if bird in matches else [match]
+          matches[bird] = matches[bird] + [match] if bird in matches else [
+            match]
     return matches
 
   def __get_bird_dictionaries(self):
@@ -57,47 +69,39 @@ class BirdSearcher:
     return [l.bird_dictionary for l in loaded_locales if l.bird_dictionary]
 
 
+class BirdSearchController:
+
+  def __init__(self, bird_searcher: BirdSearcher):
+    self.bird_searcher = bird_searcher
+
+  def search(self, name: str) -> List[BirdMatch]:
+    bird_matches = self.bird_searcher.search(name)
+    bird_matches.sort(key=lambda m: m.query_match, reverse=True)
+    return bird_matches[:100]
+
+
 class ResultBuilder:
 
   def __init__(self):
-    self.matches_by_bird = dict()
+    self.matches_by_bird: Dict[Bird, List[float]] = dict()
 
-  def add_matches(self, matches):
+  def add_matches(self, matches: Dict[Bird, List[float]]):
     if matches:
       for k, v in matches.items():
         for match in v:
           self.add_match(k, match)
 
-  def add_match(self, bird, match):
+  def add_match(self, bird: Bird, match: float):
     if bird not in self.matches_by_bird:
       self.matches_by_bird[bird] = []
     self.matches_by_bird[bird].append(match)
 
-  def create_bird_matches(self):
+  def create_bird_matches(self) -> List[BirdMatch]:
     bird_matches = []
     for bird in self.matches_by_bird:
       query_match = self.__average_query_match(bird)
       bird_matches.append(BirdMatch(bird, query_match))
     return bird_matches
 
-  def __average_query_match(self, bird):
+  def __average_query_match(self, bird: Bird) -> float:
     return sum(self.matches_by_bird[bird]) / len(self.matches_by_bird[bird])
-
-class BirdMatch:
-
-  def __init__(self, bird, query_match):
-    self.__bird = bird
-    self.__query_match = query_match
-
-  @property
-  def bird(self):
-    return self.__bird
-
-  @property
-  def query_match(self):
-    return self.__query_match
-
-class StringMatcher:
-
-  def match(self, a, b):
-    return SequenceMatcher(None, a.lower(), b.lower()).ratio()
