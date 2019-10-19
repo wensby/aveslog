a284ed269242cde9235fe951ee78c2866432c5f4
Lukas J. Wensby <wensby@gmail.com>
2019-10-19 21:38:15 +0900

    Show spinner while loading username list
    It doesn't look nice when navigating to the home page while logged in, and you
    get no indication that it is loading anything (the username list) which then
    suddenly pops into existance out of nowhere. Now, we keep a loading state in
    the home component and show the spinner while its true, and only set it to
    false once the usernames have been fetched.

--- frontend-service/src/home/Home.js
+++ frontend-service/src/home/Home.js
@@ -3,9 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { UserContext } from '../authentication/UserContext';
 import { Link } from 'react-router-dom';
 import AccountService from '../account/AccountService';
+import Spinner from '../loading/Spinner';
 
 export default () => {
   const { authenticated, getAccessToken } = useContext(UserContext);
+  const [loading, setLoading] = useState(true);
   const { t } = useTranslation();
   const [usernames, setUsernames] = useState([]);
 
@@ -14,24 +16,27 @@ export default () => {
       const accessToken = await getAccessToken();
       const accounts = await new AccountService().fetchAccounts(accessToken);
       setUsernames(accounts.map(a => a.username));
+      setLoading(false);
     }
     if (authenticated) {
       fetchAccounts();
     }
+    else {
+      setLoading(false);
+    }
   }, [authenticated, getAccessToken]);
 
-  if (authenticated) {
-    return (
-      <>
-        <h1>{t("It's birding time!")}</h1>
-        {usernames.map(name => (
-          <div key={name}>
-            <Link to={`/profile/${name}`}>{name}</Link>
-          </div>
-        ))}
-      </>
-    );
+  if (loading) {
+    return <Spinner/>;
   }
-
-  return (null);
+  return (
+    <>
+      <h1>{t("It's birding time!")}</h1>
+      {usernames.map(name => (
+        <div key={name}>
+          <Link to={`/profile/${name}`}>{name}</Link>
+        </div>
+      ))}
+    </>
+  );
 }
