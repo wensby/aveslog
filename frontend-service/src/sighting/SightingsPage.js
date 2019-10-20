ca029fa84eeddab9e589db36d14cc80857c5ebdd
Lukas J. Wensby <wensby@gmail.com>
2019-10-20 20:56:25 +0900

    Add title to sightings page

--- frontend-service/src/sighting/SightingsPage.js
+++ frontend-service/src/sighting/SightingsPage.js
@@ -2,9 +2,11 @@ import React, { useEffect, useContext } from 'react';
 import SightingList from './SightingList.js';
 import { SightingContext } from './SightingContext';
 import { SightingsStats } from './SightingsStats';
+import { useTranslation } from 'react-i18next';
 
 export default function SightingsPage() {
   const { sightings, refreshSightings } = useContext(SightingContext);
+  const { t } = useTranslation();
 
   useEffect(() => {
     refreshSightings();
@@ -12,6 +14,7 @@ export default function SightingsPage() {
 
   return (
     <>
+      <h1>{t('Sightings')}</h1>
       <SightingsStats sightings={sightings} />
       <SightingList sightings={sightings} />
     </>
