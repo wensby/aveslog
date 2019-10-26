53c5150b5facd8320307f0f03f414c0dd019e9a1
Lukas J. Wensby <wensby@gmail.com>
2019-10-26 23:42:34 +0900

    Refactor BirdCard frontend component

--- frontend-service/src/bird/BirdCard.js
+++ frontend-service/src/bird/BirdCard.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import BirdCardPicture from './BirdCardPicture';
 
-export default ({bird, key, children}) => {
-
+export default function BirdCard({ bird, key, children }) {
   const formattedName = bird.binomialName.toLowerCase().replace(' ', '-');
 
-  return (<div key={key} className="card">
-    <div className="card-horizontal">
-      <div className="img-square-wrapper">
-        <Link to={`/bird/${formattedName}`}>
-          <BirdCardPicture bird={bird} />
-        </Link>
+  return (
+    <div key={key} className="card">
+      <div className="card-horizontal">
+        <div className="img-square-wrapper">
+          <Link to={`/bird/${formattedName}`}>
+            <BirdCardPicture bird={bird} />
+          </Link>
+        </div>
+        {children}
       </div>
-      {children}
     </div>
-  </div>);
+  );
 };
