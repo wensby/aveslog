ab2868195f079a307602838b8157c0c6398a6dab
Lukas J. Wensby <wensby@gmail.com>
2019-10-28 23:36:12 +0900

    Refactor TestGetSightings to use setUp

--- api-service/src/tests/test_sighting_rest_api.py
+++ api-service/src/tests/test_sighting_rest_api.py
@@ -8,15 +8,85 @@ from test_util import AppTestCase
 
 class TestGetSightings(AppTestCase):
 
-  def test_get_sightings(self):
+  def setUp(self):
+    super().setUp()
     self.db_insert_bird(1, 'Pica pica')
     self.db_setup_account(1, 1, 'hulot', 'myPassword', 'hulot@mail.com')
+    self.db_setup_account(2, 2, 'dude', 'myPassword', 'dude@mail.com')
     self.db_insert_sighting(1, 1, 1, date(2019, 8, 28), time(11, 52))
     self.db_insert_sighting(2, 1, 1, date(2019, 8, 28), time(11, 52))
+    self.db_insert_sighting(3, 2, 1, date(2019, 8, 28), time(11, 52))
+
+  def test_get_sightings(self):
     token = self.create_access_token(1)
     headers = {'accessToken': token.jwt}
 
     response = self.client.get('/sighting', headers=headers)
+
+    self.assertEqual(response.status_code, HTTPStatus.OK)
+    self.assertEqual(response.json, {
+      'items': [
+        {
+          'sightingId': 1,
+          'personId': 1,
+          'birdId': 1,
+          'date': '2019-08-28',
+          'time': '11:52:00'
+        },
+        {
+          'sightingId': 2,
+          'personId': 1,
+          'birdId': 1,
+          'date': '2019-08-28',
+          'time': '11:52:00'
+        },
+        {
+          'sightingId': 3,
+          'personId': 2,
+          'birdId': 1,
+          'date': '2019-08-28',
+          'time': '11:52:00'
+        }
+      ],
+      'hasMore': False,
+    })
+
+  def test_get_sightings_with_valid_limit(self):
+    token = self.create_access_token(1)
+    headers = {'accessToken': token.jwt}
+
+    response = self.client.get('/sighting?limit=1', headers=headers)
+
+    self.assertEqual(response.status_code, HTTPStatus.OK)
+    self.assertEqual(response.json, {
+      'items': [
+        {
+          'sightingId': 1,
+          'personId': 1,
+          'birdId': 1,
+          'date': '2019-08-28',
+          'time': '11:52:00'
+        }
+      ],
+      'hasMore': True,
+    })
+
+  def test_get_sightings_with_invalid_limit(self):
+    token = self.create_access_token(1)
+    headers = {'accessToken': token.jwt}
+
+    response = self.client.get('/sighting?limit=0', headers=headers)
+
+    self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+    self.assertEqual(response.json, {
+      'error': 'limit-invalid',
+    })
+
+  def test_get_own_sightings_with_valid_auth_token(self):
+    token = self.create_access_token(1)
+    headers = {'accessToken': token.jwt}
+
+    response = self.client.get('/profile/hulot/sighting', headers=headers)
 
     self.assertEqual(response.status_code, HTTPStatus.OK)
     self.assertEqual(response.json, {
@@ -39,71 +109,7 @@ class TestGetSightings(AppTestCase):
       'hasMore': False,
     })
 
-  def test_get_sightings_with_valid_limit(self):
-    self.db_insert_bird(1, 'Pica pica')
-    self.db_setup_account(1, 1, 'hulot', 'myPassword', 'hulot@mail.com')
-    self.db_insert_sighting(1, 1, 1, date(2019, 8, 28), time(11, 52))
-    self.db_insert_sighting(2, 1, 1, date(2019, 8, 28), time(11, 52))
-    token = self.create_access_token(1)
-    headers = {'accessToken': token.jwt}
-
-    response = self.client.get('/sighting?limit=1', headers=headers)
-
-    self.assertEqual(response.status_code, HTTPStatus.OK)
-    self.assertEqual(response.json, {
-      'items': [
-        {
-          'sightingId': 1,
-          'personId': 1,
-          'birdId': 1,
-          'date': '2019-08-28',
-          'time': '11:52:00'
-        }
-      ],
-      'hasMore': True,
-    })
-
-  def test_get_sightings_with_invalid_limit(self):
-    self.db_insert_bird(1, 'Pica pica')
-    self.db_setup_account(1, 1, 'hulot', 'myPassword', 'hulot@mail.com')
-    self.db_insert_sighting(1, 1, 1, date(2019, 8, 28), time(11, 52))
-    token = self.create_access_token(1)
-    headers = {'accessToken': token.jwt}
-
-    response = self.client.get('/sighting?limit=0', headers=headers)
-
-    self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
-    self.assertEqual(response.json, {
-      'error': 'limit-invalid',
-    })
-
-  def test_get_own_sightings_with_valid_auth_token(self):
-    self.db_setup_account(1, 1, 'hulot', 'myPassword', 'hulot@mail.com')
-    self.db_setup_account(2, 2, 'dude', 'myPassword', 'dude@mail.com')
-    self.db_insert_bird(1, 'Pica pica')
-    self.db_insert_sighting(1, 1, 1, date(2019, 8, 28), time(11, 52))
-    self.db_insert_sighting(2, 2, 1, date(2019, 8, 28), time(12, 10))
-    token = self.create_access_token(1)
-    headers = {'accessToken': token.jwt}
-
-    response = self.client.get('/profile/hulot/sighting', headers=headers)
-
-    self.assertEqual(response.status_code, HTTPStatus.OK)
-    self.assertEqual(response.json, {
-      'items': [
-        {
-          'sightingId': 1,
-          'personId': 1,
-          'birdId': 1,
-          'date': '2019-08-28',
-          'time': '11:52:00'
-        }
-      ],
-      'hasMore': False,
-    })
-
   def test_get_sightings_when_username_missing(self):
-    self.db_setup_account(1, 1, 'hulot', 'myPassword', 'hulot@mail.com')
     token = self.create_access_token(1)
     headers = {'accessToken': token.jwt}
 
@@ -112,10 +118,6 @@ class TestGetSightings(AppTestCase):
     self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
   def test_get_sightings_when_unauthorized(self):
-    self.db_setup_account(1, 1, 'hulot', 'myPassword', 'hulot@mail.com')
-    self.db_insert_bird(1, 'Pica pica')
-    self.db_insert_sighting(1, 1, 1, date(2019, 8, 28), time(11, 52))
-
     response = self.client.get('/profile/hulot/sighting')
 
     self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED)
@@ -136,11 +138,6 @@ class TestGetSightings(AppTestCase):
     })
 
   def test_get_sightings_with_auth_token_for_other_account(self):
-    self.db_setup_account(1, 1, 'hulot', 'myPassword', 'hulot@mail.com')
-    self.db_setup_account(2, 2, 'dude', 'myPassword', 'dude@mail.com')
-    self.db_insert_bird(1, 'Pica pica')
-    self.db_insert_sighting(1, 1, 1, date(2019, 8, 28), time(11, 52))
-    self.db_insert_sighting(2, 2, 1, date(2019, 8, 28), time(12, 10))
     token = self.create_access_token(1)
     headers = {'accessToken': token.jwt}
 
@@ -150,11 +147,11 @@ class TestGetSightings(AppTestCase):
     self.assertEqual(response.json, {
       'items': [
         {
-          'sightingId': 2,
+          'sightingId': 3,
           'personId': 2,
           'birdId': 1,
           'date': '2019-08-28',
-          'time': '12:10:00'
+          'time': '11:52:00'
         }
       ],
       'hasMore': False,
