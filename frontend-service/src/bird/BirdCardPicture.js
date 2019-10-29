cc0aa57e44c4c33232d5061d72fecafd067ff537
Lukas J. Wensby <wensby@gmail.com>
2019-10-29 19:09:10 +0900

    Refactor BirdCardPicture component

--- frontend-service/src/bird/BirdCardPicture.js
+++ frontend-service/src/bird/BirdCardPicture.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import placeholder from './placeholder-bird.jpg';
 
-export default ({ bird, ...other }) => {
+export default function BirdCardPicture({ bird, ...other }) {
   const src = (bird && bird.thumbnailUrl) || placeholder;
   return <img className='bird-picture' src={src} alt="Card" {...other} />;
 };
