40729192c7191d8147639c32b6e91dac891f5292
Lukas J. Wensby <wensby@gmail.com>
2019-10-30 11:38:33 +0900

    Fix issues with footer being offset

--- frontend-service/src/Page.js
+++ frontend-service/src/Page.js
@@ -2,23 +2,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import React from 'react';
 import Navbar from './navbar/Navbar.js';
-import SideMenu from './SideMenu.js';
+import Sidebar from './SideMenu.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import './style.scss';
-
-const Between = () => (
-  <div className='between'>
-    <SideMenu />
-    <Main />
-  </div>
-);
 
 export default () => {
   return (
     <>
       <Navbar />
-      <Between />
+      <Sidebar />
+      <Main />
       <Footer />
     </>
   );
