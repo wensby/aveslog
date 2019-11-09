8f5ee5327b25d9091534d3a313ec6daf844cd4b7
Lukas J. Wensby <wensby@gmail.com>
2019-11-09 19:33:16 +0900

    Refactor SearchApi

--- api-service/src/birding/v0/search_api.py
+++ api-service/src/birding/v0/search_api.py
@@ -23,23 +23,23 @@ class SearchApi:
     self.picture_repository = picture_repository
     self.link_factory = link_factory
 
-  def result_item(self, match: BirdSearchMatch):
-    bird = match.bird
-    item = bird_summary_representation(bird)
-    item['score'] = match.score
-    if bird.thumbnail:
-      item['thumbnail'] = self.external_picture_url(bird.thumbnail.picture)
-    return item
-
   def search_birds(self, query, page_size=30) -> RestApiResponse:
     page_size = page_size if page_size else 30
     search_matches = self.bird_searcher.search(query)
     search_matches.sort(key=lambda m: m.score, reverse=True)
-    bird_matches = list(map(self.result_item, search_matches[:page_size]))
+    bird_matches = list(map(self._result_item, search_matches[:page_size]))
     return RestApiResponse(HTTPStatus.OK, {
       'items': bird_matches,
     })
 
-  def external_picture_url(self, picture: Picture) -> str:
+  def _result_item(self, match: BirdSearchMatch) -> dict:
+    bird = match.bird
+    item = bird_summary_representation(bird)
+    item['score'] = match.score
+    if bird.thumbnail:
+      item['thumbnail'] = self._external_picture_url(bird.thumbnail.picture)
+    return item
+
+  def _external_picture_url(self, picture: Picture) -> str:
     static_picture_url = os.path.join('/static/', picture.filepath)
     return self.link_factory.create_url_external_link(static_picture_url)
