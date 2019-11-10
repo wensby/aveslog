f39d48085e2a39fbdb51de65ee30163e6b6841aa
Lukas J. Wensby <wensby@gmail.com>
2019-11-10 21:08:43 +0900

    Omit test files from unittest coverage

--- api-service/run_tests.sh
+++ api-service/run_tests.sh
@@ -5,7 +5,7 @@ while ! nc -z test-database-service 5432; do sleep 1; done
 if [ -n "$1" ]; then
   rm .coverage
   rm -rf htmlcov
-  coverage run --source birding -m unittest discover -s birding
+  coverage run --source birding --omit=*/test_*.py -m unittest discover -s birding
   coverage html
 else
   python -m unittest discover -s birding
