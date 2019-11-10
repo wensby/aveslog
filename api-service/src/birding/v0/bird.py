4fd7ad611be6c38b20b469002175488387debb12
Lukas J. Wensby <wensby@gmail.com>
2019-11-10 21:47:59 +0900

    Remove unused methods in BirdRepository

--- api-service/src/birding/v0/bird.py
+++ api-service/src/birding/v0/bird.py
@@ -8,12 +8,6 @@ class BirdRepository:
   def __init__(self, sqlalchemy_session: Session):
     self.session = sqlalchemy_session
 
-  def bird_thumbnail(self, bird: Bird) -> Optional[BirdThumbnail]:
-    return self.session.query(BirdThumbnail).filter_by(bird_id=bird.id).first()
-
-  def get_bird_by_id(self, id: int) -> Optional[Bird]:
-    return self.session.query(Bird).get(id)
-
   def get_bird_by_binomial_name(self, binomial_name: str) -> Optional[Bird]:
     return self.session.query(Bird).filter(
       Bird.binomial_name.ilike(binomial_name)).first()
