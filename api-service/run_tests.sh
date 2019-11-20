2216a7da935feaac649fcee05c3eccba18d96212
Lukas J. Wensby <wensby@gmail.com>
2019-11-20 13:43:26 +0900

    Fail unit tests early

--- api-service/run_tests.sh
+++ api-service/run_tests.sh
@@ -5,8 +5,8 @@ while ! nc -z test-database-service 5432; do sleep 1; done
 if [ -n "$1" ]; then
   rm .coverage
   rm -rf htmlcov
-  coverage run --omit=*/test_*.py -m unittest discover
+  coverage run --omit=*/test_*.py -m unittest discover -f
   coverage html
 else
-  python -m unittest discover -s aveslog
+  python -m unittest discover -f -s aveslog
 fi
