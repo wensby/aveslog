54a4f0475894d25bab7f693ddc54f861d5364085
Lukas J. Wensby <wensby@gmail.com>
2019-11-30 21:28:40 +0900

    Unit test BirdName database model

--- api-service/src/aveslog/v0/test_models.py
+++ api-service/src/aveslog/v0/test_models.py
@@ -2,9 +2,17 @@ from datetime import date, time
 from unittest import TestCase
 
 from aveslog.v0.models import Birder
+from aveslog.v0.models import BirdName
 from aveslog.v0.models import PasswordResetToken
 from aveslog.v0.models import Picture
 from aveslog.v0.models import Sighting
+
+
+class TestBirdName(TestCase):
+
+  def test_repr(self):
+    bird_name = BirdName(name='Skata')
+    self.assertEqual(repr(bird_name), "<BirdName(name='Skata')>")
 
 
 class TestBirder(TestCase):
