854d1a010c79c4263b455c264bd0b1f9455928c1
Lukas J. Wensby <wensby@gmail.com>
2020-01-05 18:52:06 +0900

    Add user agent to nominatim API calls

--- api-service/src/aveslog/nominatim.py
+++ api-service/src/aveslog/nominatim.py
@@ -6,7 +6,9 @@ class Nominatim:
     self._url = 'https://nominatim.openstreetmap.org'
 
   def reverse(self, lat, lon, zoom=18, language='en-EN'):
-    r = requests.get(f'{self._url}/reverse?format=jsonv2&lat={lat}&lon={lon}&zoom={zoom}&accept-language={language}')
+    r = requests.get(f'{self._url}/reverse?format=jsonv2&lat={lat}&lon={lon}&zoom={zoom}&accept-language={language}', headers={
+      'User-Agent': 'Aveslog.com'
+    })
     if r.status_code == 200:
       return r.json()
     else:
