7fa5267e59fc0e5a1f03d0c4b21c1c65fa667e29
Lukas J. Wensby <wensby@gmail.com>
2020-01-13 14:33:16 +0900

    Remove image backup and restoration
    Images are now not stored and served from the api service, but externally.

--- backup/backup.sh
+++ backup/backup.sh
@@ -5,6 +5,5 @@ backup_dir=$(date '+%Y%m%d_%H%M%S')
 mkdir $backup_dir
 
 # Backup web service images
-docker cp api-service:/src/aveslog/static/image $backup_dir
 docker exec database-service \
   pg_dump -c -U postgres birding-database > $backup_dir/dump.sql
--- backup/restore.sh
+++ backup/restore.sh
@@ -5,6 +5,5 @@ if [ -z "$1" ]; then
 fi
 
 backup_dir=$1
-docker cp $backup_dir/image/* api-service:/src/aveslog/static/image/
 cat $backup_dir/dump.sql | docker exec -i database-service \
   psql -U postgres -d birding-database
