7204dd7caf6f4e68293d71e5d8f1c39814770014
Lukas J. Wensby <wensby@gmail.com>
2020-01-14 10:45:59 +0900

    Use placeholder in case of bird picture error

--- frontend-service/src/bird/BirdCardPicture.js
+++ frontend-service/src/bird/BirdCardPicture.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import placeholder from './placeholder-bird.jpg';
 
 export default function BirdCardPicture({ bird, ...other }) {
-  const src = ((bird || {}).thumbnail || {}).url || placeholder;
-  return <img className='bird-picture' src={src} alt="Card" {...other} />;
+  const [src, setSrc] = useState(((bird || {}).thumbnail || {}).url || placeholder);
+  return <img
+    className='bird-picture'
+    onError={() => setSrc(placeholder)}
+    src={src}
+    alt="Card" {...other}
+  />;
 };
