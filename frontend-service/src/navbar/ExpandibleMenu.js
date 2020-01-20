309fe364f2c2607dc1fe2e1a397ddcc8fb6b1f13
Lukas J. Wensby <wensby@gmail.com>
2020-01-20 14:12:51 +0900

    Refactor ExpandibleMenu frontend component

--- frontend-service/src/navbar/ExpandibleMenu.js
+++ frontend-service/src/navbar/ExpandibleMenu.js
@@ -4,16 +4,16 @@ import { UserContext } from '../authentication/UserContext.js';
 import { getMenuItems } from './MenuItemsFactory.js';
 import { useTranslation } from 'react-i18next';
 
-export function ExpandibleMenu({ collapseState, onMenuClick }) {
+export function ExpandibleMenu({ state, onItemClick }) {
   const { authenticated, account, unauthenticate } = useContext(UserContext);
   const { t } = useTranslation();
   const items = getMenuItems(authenticated, account, unauthenticate, t);
-  const needMaxHeight = ['expanding', 'expanded'].indexOf(collapseState) >= 0;
+  const needMaxHeight = ['expanding', 'expanded'].indexOf(state) >= 0;
   const style = needMaxHeight ? { maxHeight: `${items.length * 37}px` } : {};
 
   return (
-    <div className={`menu ${collapseState}`} style={style}>
-      <Menu items={items} onClick={onMenuClick} />
+    <div className={`menu ${state}`} style={style}>
+      <Menu items={items} onClick={onItemClick} />
     </div>
   );
 }
