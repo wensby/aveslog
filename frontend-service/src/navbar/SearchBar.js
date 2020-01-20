21769cd530551338389803aa96335648df5972f4
Lukas J. Wensby <wensby@gmail.com>
2020-01-20 10:33:31 +0900

    Add advanced search toggle button
    Currently, this toggle button doesn't do anything except toggling. But soon it
    will cause an expanded search section to expand making it possible for the
    user to select more options when it comes to searching for birds.

--- frontend-service/src/navbar/SearchBar.js
+++ frontend-service/src/navbar/SearchBar.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { useReactRouter } from '../reactRouterHook';
 import { useTranslation } from 'react-i18next';
+import Icon from '../Icon.js';
 
 export function SearchBar({}) {
   const { history } = useReactRouter();
   const [query, setQuery] = useState('');
+  const [advanced, setAdvanced] = useState(false);
   const { t } = useTranslation();
 
   const syncQueryState = event => {
@@ -22,8 +24,24 @@ export function SearchBar({}) {
       <div className='search-bar'>
         <input className='form-input' placeholder={t('Search bird')} 
           aria-label='Search bird' onChange={syncQueryState} value={query} />
+        <AdvancedSearchToggle active={advanced} onChange={setAdvanced} />
         <button type='submit'>{t('Search')}</button>
       </div>
     </form>
+  );
+}
+
+function AdvancedSearchToggle({ active, onChange }) {
+  const advancedClick = e => {
+    e.preventDefault();
+    onChange(!active);
+  };
+
+  const className = 'search-advanced-icon' + (active ? ' expanded' : '');
+
+  return (
+    <div className={className} onClick={advancedClick}>
+      <Icon name='down' />
+    </div>
   );
 }
