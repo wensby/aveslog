2778e0dc64fd7e29cdf286b6599edd22c2bbf754
Lukas J. Wensby <wensby@gmail.com>
2020-01-21 14:32:24 +0900

    Refactor BirdPage

--- frontend-service/src/bird/BirdPage.js
+++ frontend-service/src/bird/BirdPage.js
@@ -11,37 +11,20 @@ export function BirdPage({ match }) {
   const bird = useBird(match.params.binomialName);
   const stats = useBirdStatistics(bird);
 
-  const renderPhotoCredits = () => {
-    if (bird.thumbnail) {
-      return (
-        <div>
-          <p><small>{`Thumbnail Photo by: ${bird.thumbnail.credit}`}</small></p>
-        </div>
-      );
-    }
-  }
-
-  const renderAddSighting = () => {
-    if (account) {
-      return <NewBirdSightingLink bird={bird}>{t('add-sighting-link')}</NewBirdSightingLink>;
-    }
-  }
-
-  const renderStats = () => {
-    return <div>
-      Sightings: {stats.sightingsCount}
-      <br />
-      Lucky birders: {stats.birdersCount}
-    </div>;
-  }
-
   if (bird) {
     return (
       <div>
         <BirdCover bird={bird} />
-        {renderStats()}
-        {renderPhotoCredits()}
-        {renderAddSighting()}
+        <div>
+          Sightings: {stats.sightingsCount}
+          <br />
+          Lucky birders: {stats.birdersCount}
+        </div>
+        {bird.thumbnail && 
+        <div>
+          <p><small>{`Thumbnail Photo by: ${bird.thumbnail.credit}`}</small></p>
+        </div>}
+        {account && <NewBirdSightingLink bird={bird}>{t('add-sighting-link')}</NewBirdSightingLink>}
       </div>
     );
   }
