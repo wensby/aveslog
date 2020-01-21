3f7cdd2a708bdb8ea17d1246fe945bbbf1db219b
Lukas J. Wensby <wensby@gmail.com>
2020-01-21 09:48:56 +0900

    Extract SightingFeedItemContainer to new file

--- frontend-service/src/home/SightingFeed.js
+++ frontend-service/src/home/SightingFeed.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { useBird } from '../bird/BirdHooks';
-import { useBirder } from '../birder/BirderHooks';
-import { SightingFeedItem } from './SightingFeedItem';
+import { SightingFeedItemContainer } from './SightingFeedItemContainer';
 
 export function SightingFeed({ sightings }) {
   return <>{sightings.map(sighting => <SightingFeedItemContainer sighting={sighting} />)}</>;
-}
-
-function SightingFeedItemContainer({ sighting }) {
-  const bird = useBird(sighting.birdId);
-  const birder = useBirder(sighting.birderId);
-  if (!bird || !birder) {
-    return null;
-  }
-  return <SightingFeedItem sighting={sighting} bird={bird} birder={birder} />;
 }
---
+++ frontend-service/src/home/SightingFeedItemContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useBird } from '../bird/BirdHooks';
+import { useBirder } from '../birder/BirderHooks';
+import { SightingFeedItem } from './SightingFeedItem';
+
+export function SightingFeedItemContainer({ sighting }) {
+  const bird = useBird(sighting.birdId);
+  const birder = useBirder(sighting.birderId);
+
+  if (!bird || !birder) {
+    return null;
+  }
+  
+  return <SightingFeedItem sighting={sighting} bird={bird} birder={birder} />;
+}
