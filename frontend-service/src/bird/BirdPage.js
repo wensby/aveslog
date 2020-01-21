23871211cf6dafcb99e58c8aecbf47b9caa39169
Lukas J. Wensby <wensby@gmail.com>
2020-01-21 14:35:55 +0900

    Extract component DetailedBirdCard to new file

--- frontend-service/src/bird/BirdPage.js
+++ frontend-service/src/bird/BirdPage.js
@@ -1,34 +1,8 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import NewBirdSightingLink from '../sighting/NewBirdSightingLink';
-import { useBird, useBirdStatistics } from './BirdHooks';
-import { BirdCover } from './BirdCover.js';
-import { useAuthentication } from '../authentication/AuthenticationHooks';
+import { useBird } from './BirdHooks';
+import { DetailedBirdCard } from './DetailedBirdCard';
 
 export function BirdPage({ match }) {
-  const { t } = useTranslation();
-  const { account } = useAuthentication()
   const bird = useBird(match.params.binomialName);
-  const stats = useBirdStatistics(bird);
-
-  if (bird) {
-    return (
-      <div>
-        <BirdCover bird={bird} />
-        <div>
-          Sightings: {stats.sightingsCount}
-          <br />
-          Lucky birders: {stats.birdersCount}
-        </div>
-        {bird.thumbnail && 
-        <div>
-          <p><small>{`Thumbnail Photo by: ${bird.thumbnail.credit}`}</small></p>
-        </div>}
-        {account && <NewBirdSightingLink bird={bird}>{t('add-sighting-link')}</NewBirdSightingLink>}
-      </div>
-    );
-  }
-  else {
-    return null;
-  }
+  return bird ? <DetailedBirdCard bird={bird} /> : null;
 }
---
+++ frontend-service/src/bird/DetailedBirdCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import NewBirdSightingLink from '../sighting/NewBirdSightingLink';
+import { useBirdStatistics } from './BirdHooks';
+import { BirdCover } from './BirdCover.js';
+import { useAuthentication } from '../authentication/AuthenticationHooks';
+
+export function DetailedBirdCard({ bird }) {
+  const { t } = useTranslation();
+  const { account } = useAuthentication();
+  const stats = useBirdStatistics(bird);
+
+  return (
+    <div>
+      <BirdCover bird={bird} />
+      <div>
+        Sightings: {stats.sightingsCount}
+        <br />
+        Lucky birders: {stats.birdersCount}
+      </div>
+      {bird.thumbnail &&
+        <div>
+          <p><small>{`Thumbnail Photo by: ${bird.thumbnail.credit}`}</small></p>
+        </div>}
+      {account && <NewBirdSightingLink bird={bird}>{t('add-sighting-link')}</NewBirdSightingLink>}
+    </div>
+  );
+}
