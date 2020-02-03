69196082a0a652a6d059bc75b6fa774e0d4d1e4e
Lukas J. Wensby <wensby@gmail.com>
2020-02-03 15:13:59 +0900

    Lazy load sighting list items

--- frontend-service/src/sighting/SightingList.js
+++ frontend-service/src/sighting/SightingList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { SightingCard } from './SightingCard.js';
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 export function SightingList({ sightings }) {
 
   const renderSightingCard = sighting => {
     return (
-      <React.Fragment key={sighting.id}>
+      <LazyLoadComponent placeholder={<div style={{height: '150px'}}></div>}><React.Fragment key={sighting.id}>
         <SightingCard sighting={sighting} key={sighting.id} />
-      </React.Fragment>
+      </React.Fragment></LazyLoadComponent>
     );
   };
 
