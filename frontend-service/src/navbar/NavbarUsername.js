a05be84b5e8611e663efc2dabf574fae19a6eae5
Lukas J. Wensby <wensby@gmail.com>
2020-02-13 14:23:07 +0900

    Clean up NavbarUsername.js

--- frontend-service/src/navbar/NavbarUsername.js
+++ frontend-service/src/navbar/NavbarUsername.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const NavbarUsername = ({username}) => (
   <div className='navbar-username'>
