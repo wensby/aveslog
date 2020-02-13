c790fa0d6478e64dd9fd056ed02effb40c4b3fb3
Lukas J. Wensby <wensby@gmail.com>
2020-02-13 14:25:13 +0900

    Extract NavbarUsername.scss from Navbar.scss

--- frontend-service/src/navbar/NavbarUsername.js
+++ frontend-service/src/navbar/NavbarUsername.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavbarUsername.scss';
 
 export const NavbarUsername = ({username}) => (
   <div className='navbar-username'>
