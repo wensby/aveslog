75be05fff2095b85ec455e3e519a519e6529b819
Lukas J. Wensby <wensby@gmail.com>
2020-02-13 18:34:29 +0900

    Hide search button until input available
    This makes the mobile experience a little more slick and not as cluttered. On
    the desktop version, though, the search button is always visible.

--- frontend-service/src/navbar/SearchBar.js
+++ frontend-service/src/navbar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from "react-router-dom";
 import { AdvancedSearchToggle } from './AdvancedSearchToggle';
@@ -8,24 +8,36 @@ export const SearchBar = () => {
   const history = useHistory();
   const [query, setQuery] = useState('');
   const [advanced, setAdvanced] = useState(false);
+  const [searchButtonVisible, setSearchButtonVisible] = useState(false);
   const { t } = useTranslation();
+  const inputRef = useRef(null);
 
   const syncQueryState = event => {
     setQuery(event.target.value);
   }
 
+  useEffect(() => {
+    if (query || advanced) {
+      setSearchButtonVisible(true);
+    }
+    else {
+      setSearchButtonVisible(false);
+    }
+  }, [query, advanced]);
+
   const onFormSubmit = event => {
     event.preventDefault();
     history.push(`/bird/search?q=${query}`);
+    inputRef.current.blur();
   }
 
   return (
     <form onSubmit={onFormSubmit}>
       <div className='search-bar'>
-        <input className='form-input' placeholder={t('Search bird')} 
+        <input ref={inputRef} className='form-input' placeholder={t('Search bird')}
           aria-label='Search bird' onChange={syncQueryState} value={query} />
         <AdvancedSearchToggle active={advanced} onChange={setAdvanced} />
-        <button type='submit'>{t('Search')}</button>
+        <button className={searchButtonVisible ? 'expanded' : ''} type='submit'>{t('Search')}</button>
       </div>
     </form>
   );
