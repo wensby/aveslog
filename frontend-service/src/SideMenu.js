3fcb28f19b2c7382e9497c1a84ceca5ac2e40100
Lukas J. Wensby <wensby@gmail.com>
2020-02-18 17:38:12 +0900

    Use single quotes in SideMenu component

--- frontend-service/src/SideMenu.js
+++ frontend-service/src/SideMenu.js
@@ -6,12 +6,12 @@ export const SideMenu = () => {
   const items = useMenuItems();
 
   const renderItem = (item, index) => {
-    return <div className="nav-item" key={index}>{item}</div>;
+    return <div className='nav-item' key={index}>{item}</div>;
   };
 
   return (
-    <div className="sidemenu">
-      <nav className="sidebar nav flex-column border-left" id="sidebarList">
+    <div className='sidemenu'>
+      <nav className='sidebar nav flex-column border-left' id='sidebarList'>
         {items.map(renderItem)}
       </nav>
     </div>
