9900449edfb702c3d21ded7cb30611c17e6c5697
Lukas J. Wensby <wensby@gmail.com>
2020-02-20 00:10:45 +0900

    Clean up frontend Label component

--- frontend-service/src/sighting/Label.js
+++ frontend-service/src/sighting/Label.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useTranslation } from "react-i18next";
-export function Label({ htmlFor, label }) {
+import { useTranslation } from 'react-i18next';
+
+export const Label = ({ htmlFor, label }) => {
   const { t } = useTranslation();
   return <label htmlFor={htmlFor}>{t(label)}</label>;
-}
+};
