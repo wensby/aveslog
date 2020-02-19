23cdc164b0abe33931eec2dbdcfc97b82613adc7
Lukas J. Wensby <wensby@gmail.com>
2020-02-19 14:12:24 +0900

    Refactor LoginPage component

--- frontend-service/src/authentication/LoginPage.js
+++ frontend-service/src/authentication/LoginPage.js
@@ -19,25 +19,23 @@ const NewAccountSection = () => {
 }
 
 export const LoginPage = () => {
-  const [loginErrorMessage, setLoginErrorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
   const { t } = useTranslation();
 
-  const setErrorMessage = message => {
-    setLoginErrorMessage(message);
-  }
-
-  return <div className='login'>
-    <h1>{t('Login')}</h1>
-    {loginErrorMessage && <ErrorMessage message={loginErrorMessage} />}
-    <LoginForm onError={setErrorMessage} />
-    <div className='password-recover-link'>
-      <Link to='/authentication/password-reset'>
-        {t('password-recover-link')}
-      </Link>
-    </div>
-    <Separator />
-    <NewAccountSection />
-  </div>;
+  return (
+    <div className='login'>
+      <h1>{t('Login')}</h1>
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      <LoginForm onError={setErrorMessage} />
+      <div className='password-recover-link'>
+        <Link to='/authentication/password-reset'>
+          {t('password-recover-link')}
+        </Link>
+      </div>
+      <Separator />
+      <NewAccountSection />
+    </div>)
+    ;
 };
 
 function ErrorMessage({ message }) {
