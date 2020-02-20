266103a82381e0c72a426703d20bc8bf104818ec
Lukas J. Wensby <wensby@gmail.com>
2020-02-20 19:16:58 +0900

    Extract BirdCardPicture.scss from style.scss

--- frontend-service/src/bird/BirdCardPicture.js
+++ frontend-service/src/bird/BirdCardPicture.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import placeholder from './placeholder-bird.jpg';
+import './BirdCardPicture.scss';
 
 export function BirdCardPicture({ bird, ...other }) {
   const [src, setSrc] = useState(((bird || {}).thumbnail || {}).url || placeholder);
