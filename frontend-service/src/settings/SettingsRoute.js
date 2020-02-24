49e2d5b7a90367a71d1b22e4313a37d61872c36e
Lukas J. Wensby <wensby@gmail.com>
2020-02-24 17:52:56 +0900

    Stop default exporting Settings component

--- frontend-service/src/settings/SettingsRoute.js
+++ frontend-service/src/settings/SettingsRoute.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useTranslation } from "react-i18next";
 import { PasswordChangeForm } from './PasswordChangeForm';
 
-export default function Settings() {
+export const Settings = () => {
   const { t } = useTranslation();
-  return (<div>
-    <h1>{t('Settings')}</h1>
-    <h2>{t('change-password')}</h2>
-    <PasswordChangeForm />
-  </div>);
+  return (
+    <div>
+      <h1>{t('Settings')}</h1>
+      <h2>{t('change-password')}</h2>
+      <PasswordChangeForm />
+    </div>
+  );
 }
