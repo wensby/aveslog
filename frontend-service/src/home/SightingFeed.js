99f7f59f1602c3f24610ce6fb8dd52e1be58c585
Lukas J. Wensby <wensby@gmail.com>
2020-02-26 16:04:31 +0900

    Refactor and reformat SightingFeed component

--- frontend-service/src/home/SightingFeed.js
+++ frontend-service/src/home/SightingFeed.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { SightingFeedItemContainer } from './SightingFeedItemContainer';
 import { DateDelimiter } from './DateDelimiter';
 
-export function SightingFeed({ sightings }) {
+export const SightingFeed = ({ sightings }) => {
   let date = null;
   const elements = [];
+  
   sightings.forEach((sighting, index) => {
     const sightingDate = new Date(sighting.date);
     if (date === null || sightingDate < date) {
-      elements.push(<DateDelimiter date={sighting.date} key={index+'date'} />);
+      elements.push(<DateDelimiter date={sighting.date} key={index + 'date'} />);
       date = sightingDate;
     }
     elements.push(<SightingFeedItemContainer sighting={sighting} key={index} />);
   });
+
   return <>{elements}</>;
-}
+};
