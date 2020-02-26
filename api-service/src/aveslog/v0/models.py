5803fb639913d2d939e763ac2dbf35f335d64399
Lukas J. Wensby <wensby@gmail.com>
2020-02-26 19:44:51 +0900

    Remove unused backend import

--- api-service/src/aveslog/v0/models.py
+++ api-service/src/aveslog/v0/models.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Table
 from sqlalchemy import Integer
