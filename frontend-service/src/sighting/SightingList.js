a246285b7ab158e6843849542b086f18438a77e5
Lukas J. Wensby <wensby@gmail.com>
2020-03-01 14:59:40 +0900

    Refactor SightingList

--- frontend-service/src/sighting/SightingList.js
+++ frontend-service/src/sighting/SightingList.js
@@ -3,14 +3,17 @@ import { SightingCard, SightingCardPlaceholder } from './SightingCard.js';
 import { LazyLoad } from '../LazyLoad.js';
 
 export const SightingList = ({ sightings }) => {
-
-  const renderSightingCard = sighting => {
-    return (
-      <LazyLoad offset={1000} key={sighting.id} placeholder={<SightingCardPlaceholder />}>
-        <SightingCard sighting={sighting} />
-      </LazyLoad>
-    );
-  };
-
-  return <div className='text-break'>{sightings.map(renderSightingCard)}</div>;
+  return (
+    <div className='text-break'>
+      {sightings.map(s => <LazyLoadedSightingCard sighting={s} />)}
+    </div>
+  );
 };
+
+const LazyLoadedSightingCard = ({ sighting }) => {
+  return (
+    <LazyLoad offset={1000} key={sighting.id} placeholder={<SightingCardPlaceholder />}>
+      <SightingCard sighting={sighting} />
+    </LazyLoad>
+  );
+}
