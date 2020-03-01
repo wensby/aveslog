32b606c1e7565d29cd33ca95f7069496d53868ce
Lukas J. Wensby <wensby@gmail.com>
2020-03-01 20:08:11 +0900

    Refactor and rework BirdThumbnailImage

--- frontend-service/src/bird/BirdThumbnailImage.js
+++ frontend-service/src/bird/BirdThumbnailImage.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import placeholder from './placeholder-bird.jpg';
 import './BirdThumbnailImage.scss';
 
 export const BirdThumbnailImage = ({ bird, ...other }) => {
-  const [src, setSrc] = useState(((bird || {}).thumbnail || {}).url || placeholder);
+  const [src, setSrc] = useState(placeholder);
+
+  useEffect(() => {
+    if (((bird || {}).thumbnail || {}).url) {
+      if (bird.thumbnail.url !== src) {
+        setSrc(bird.thumbnail.url);
+      }
+    }
+    else {
+      setSrc(placeholder);
+    }
+  }, [bird, src])
+
   return <img
     className='bird-thumbnail'
     onError={() => setSrc(placeholder)}
