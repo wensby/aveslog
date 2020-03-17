6cca48d3fe213b518e092e72ad6d088aaaaf1a94
Lukas J. Wensby <wensby@gmail.com>
2020-03-17 16:19:49 +0100

    Refactor and clean up FilterableSightingsList

--- frontend-service/src/sighting/FilterableSightingsList.js
+++ frontend-service/src/sighting/FilterableSightingsList.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { SightingList } from './SightingList.js';
-import { SightingsStats } from './SightingsStats';
 import { SightingsFilter } from './SightingsFilter';
+import { SightingsStats } from './SightingsStats';
+import { SightingList } from './SightingList.js';
 
-export function FilterableSightingsList({ sightings }) {
+export const FilterableSightingsList = ({ sightings }) => {
   const [filteredSightings, setFilteredSightings] = useState([]);
   const [yearFilter, setYearFilter] = useState(null);
 
   useEffect(() => {
-    if (yearFilter === null) {
-      setFilteredSightings(sightings);
+    var filtered = sightings;
+    if (yearFilter !== null) {
+      filtered = filtered
+        .filter(s => new Date(s.date).getFullYear() === yearFilter);
     }
-    else {
-      setFilteredSightings(sightings.filter(sighting => new Date(sighting.date).getFullYear() === yearFilter));
-    }
+    setFilteredSightings(filtered);
   }, [sightings, yearFilter]);
+
   return <>
     <SightingsFilter sightings={sightings} selectedYear={yearFilter} onYearChange={setYearFilter} />
     <SightingsStats sightings={filteredSightings} />
     <SightingList sightings={filteredSightings} />
   </>;
-}
+};
