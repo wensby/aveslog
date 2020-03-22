22785dd9d608ae9d0751d84e520b32cc248cc4cc
Lukas J. Wensby <wensby@gmail.com>
2020-03-22 21:37:45 +0100

    Add missing useEffect dependencies

--- frontend-service/src/birder/BirderPage.js
+++ frontend-service/src/birder/BirderPage.js
@@ -67,7 +67,7 @@ const BirderConnectionButton = ({ birder }) => {
       }
     };
     resolveFollows();
-  }, [birder]);
+  }, [birder, account.birder.id, getAccessToken]);
 
   const handleClick = e => {
     const updateBirderConnection = async () => {
