20de2d2f000356753ac64e161e99dcf391167e50
Lukas J. Wensby <wensby@gmail.com>
2020-03-22 23:34:56 +0100

    Make Spinner dimension respond to container

--- frontend-service/src/generic/Spinner.js
+++ frontend-service/src/generic/Spinner.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './Spinner.scss';
 
 export const Spinner = () => {
-  return <div className='spinner' />;
+  return (
+    <div className='spinner'>
+      <div />
+    </div>
+  );
 };
