2f5924e5983804c10b5e108abdf65fb2b494faa3
Lukas J. Wensby <wensby@gmail.com>
2020-03-23 17:47:26 +0100

    Add spinner when fetching position for search
    When about to search for sighted birds in your vicinity, it is very nice if
    there is a loading animation while the browser is resolving your position until
    the checkbox becomes checked.

--- frontend-service/src/navbar/AdvancedSearchSection.js
+++ frontend-service/src/navbar/AdvancedSearchSection.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { SearchBarContext } from './SearchBar.js';
 import { useTranslation } from 'react-i18next';
+import { Spinner } from '../generic/Spinner';
 import './AdvancedSearchSection.scss';
 
 export const AdvancedSearchSection = () => {
@@ -22,17 +23,32 @@ export const AdvancedSearchSection = () => {
 };
 
 const SightedNearbyFormGroup = () => {
-  const { positionActive, setPositionActive } = useContext(SearchBarContext);
   const { t } = useTranslation();
-
-  const handleChange = e => {
-    setPositionActive(e.target.checked);
-  }
 
   return (
     <div className='sighted-nearby-group'>
-      <input id='positionCheckbox' type='checkbox' checked={positionActive} onChange={handleChange} />
+      <SightedNearbyCheckbox />
       <label htmlFor='positionCheckbox'>{t('search-sighted-nearby-label')}</label>
     </div>
   );
 };
+
+const SightedNearbyCheckbox = () => {
+  const { positionActive, setPositionActive, position } = useContext(SearchBarContext);
+  const loading = positionActive && !position;
+
+  const handleChange = e => {
+    setPositionActive(e.target.checked);
+  }
+  
+  if (loading) {
+    return <div><Spinner /></div>;
+  }
+  else {
+    return <input
+      id='positionCheckbox'
+      type='checkbox'
+      checked={positionActive}
+      onChange={handleChange} />;
+  }
+}
