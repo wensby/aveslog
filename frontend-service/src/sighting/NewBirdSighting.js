861917c227b0d381cdb52ef554e1a7f76edf7770
Lukas J. Wensby <wensby@gmail.com>
2020-03-24 22:42:45 +0100

    Change NewBirdSighting to arrow function

--- frontend-service/src/sighting/NewBirdSighting.js
+++ frontend-service/src/sighting/NewBirdSighting.js
@@ -3,7 +3,7 @@ import { SightingSuccess } from './SightingSuccess';
 import { NewSightingForm } from './NewSightingForm';
 import { useBird } from '../bird/BirdHooks';
 
-export function NewBirdSighting({ match }) {
+export const NewBirdSighting = ({ match }) => {
   const binomialName = match.params.birdId;
   const { bird } = useBird(binomialName);
   const [addedSighting, setAddedSighting] = useState(null);
@@ -17,4 +17,4 @@ export function NewBirdSighting({ match }) {
   }
 
   return <NewSightingForm bird={bird} onSuccess={setAddedSighting}/>;
-}
+};
