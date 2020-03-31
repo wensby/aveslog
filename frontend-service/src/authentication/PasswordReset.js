6bcdcc15e1f967f38fb5985b6c91160af3c24824
Lukas J. Wensby <wensby@gmail.com>
2020-03-31 16:39:52 +0200

    Clean up PasswordReset component

--- frontend-service/src/authentication/PasswordReset.js
+++ frontend-service/src/authentication/PasswordReset.js
@@ -6,18 +6,10 @@ import { PasswordResetForm } from './PasswordResetForm.js';
 export const PasswordReset = ({ match }) => {
   const { path } = match;
 
-  const renderCreatePasswordReset = props => {
-    return <CreatePasswordReset {...props}/>
-  };
-
-  const renderPasswordResetForm = props => {
-    return <PasswordResetForm {...props} />;
-  };
-
   return (
     <Switch>
-      <Route exact path={path} render={renderCreatePasswordReset}/>
-      <Route path={`${path}/:token`} render={renderPasswordResetForm}/>
+      <Route exact path={path} component={CreatePasswordReset}/>
+      <Route path={`${path}/:token`} component={PasswordResetForm}/>
     </Switch>
   );
 };
