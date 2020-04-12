68f7c4bccfed5b239e5de9d8e22499a9ba0d2a1b
Lukas J. Wensby <wensby@gmail.com>
2020-04-12 11:27:11 +0200

    Add Nominatim unit tests

--- api-service/src/aveslog/test_nominatim.py
+++ api-service/src/aveslog/test_nominatim.py
@@ -14,11 +14,12 @@ class MockResponse:
 
 class TestNominatim(TestCase):
 
+  def setUp(self):
+    self.nominatim = Nominatim()
+
   @mock.patch('requests.get', side_effect=[MockResponse({'key': 'value'}, 200)])
   def test_reverse(self, mock_get):
-    nominatim = Nominatim()
-
-    result = nominatim.reverse(10, 9, 17, 'en-EN')
+    result = self.nominatim.reverse(10, 9, 17, 'en-EN')
 
     self.assertListEqual(mock_get.call_args_list, [
       mock.call(
@@ -27,3 +28,15 @@ class TestNominatim(TestCase):
       )
     ])
     self.assertDictEqual(result, {'key': 'value'})
+
+  @mock.patch('requests.get', side_effect=[MockResponse({}, 400)])
+  def test_reverse_when_response_not_ok(self, mock_get):
+    result = self.nominatim.reverse(10, 9, 17, 'en-EN')
+
+    self.assertListEqual(mock_get.call_args_list, [
+      mock.call(
+        'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=10&lon=9&zoom=17&accept-language=en-EN',
+        headers={'User-Agent': 'Aveslog.com'}
+      )
+    ])
+    self.assertIsNone(result)
