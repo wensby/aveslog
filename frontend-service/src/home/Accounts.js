acc799b5ec862cd21e161724697ab383091f0a36
Lukas J. Wensby <wensby@gmail.com>
2020-06-06 22:12:52 +0200

    Move birders fetching from Accounts component

--- frontend-service/src/home/Accounts.js
+++ frontend-service/src/home/Accounts.js
@@ -1,37 +1,6 @@
-import React, { useContext, useState, useEffect } from 'react';
-import Spinner from '../loading/Spinner';
-import AccountService from '../account/AccountService';
+import React from 'react';
 import { BirderLink } from './BirderLink';
-import { AuthenticationContext } from '../authentication/AuthenticationContext';
 
-export const Accounts = () => {
-  const { authenticated, getAccessToken } = useContext(AuthenticationContext);
-  const [loading, setLoading] = useState(true);
-  const [accounts, setAccounts] = useState([]);
-
-  useEffect(() => {
-    const fetchAccounts = async () => {
-      const accessToken = await getAccessToken();
-      if (accessToken) {
-        const response = await new AccountService().fetchAccounts(accessToken);
-        if (response.status === 200) {
-          const json = await response.json();
-          setAccounts(json.items);
-          setLoading(false);
-        }
-      }
-    }
-    if (authenticated) {
-      fetchAccounts();
-    }
-    else {
-      setLoading(false);
-    }
-  }, [authenticated, getAccessToken]);
-
-  if (loading) {
-    return <Spinner />;
-  }
-
+export const Accounts = ({ accounts }) => {
   return accounts.map(account => (<BirderLink key={account.username} birder={account.birder} />));
 };
--- frontend-service/src/pages/BirdersPage.js
+++ frontend-service/src/pages/BirdersPage.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { Accounts } from '../home/Accounts';
+import React, { useContext, useState, useEffect } from 'react';
+import Spinner from 'loading/Spinner';
+import AccountService from 'account/AccountService';
+import { AuthenticationContext } from '../authentication/AuthenticationContext';
+import { Accounts } from 'home/Accounts';
 
 export default () => {
+  const { authenticated, getAccessToken } = useContext(AuthenticationContext);
+  const [loading, setLoading] = useState(true);
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    const fetchAccounts = async () => {
+      const accessToken = await getAccessToken();
+      if (accessToken) {
+        const response = await new AccountService().fetchAccounts(accessToken);
+        if (response.status === 200) {
+          const json = await response.json();
+          setAccounts(json.items);
+          setLoading(false);
+        }
+      }
+    }
+    if (authenticated) {
+      fetchAccounts();
+    }
+    else {
+      setLoading(false);
+    }
+  }, [authenticated, getAccessToken]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <div>
-      <Accounts />
+      <Accounts accounts={accounts} />
     </div>
   );
 };
