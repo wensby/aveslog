dd0a7ed6d9826bc44c4462d9f02cf92bc2661515
Lukas J. Wensby <wensby@gmail.com>
2020-07-24 22:20:28 +0200

    Fix loading spinner on birder page

--- frontend-service/client/src/birder/BirderPageContainer.js
+++ frontend-service/client/src/birder/BirderPageContainer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BirderPage } from 'pages';
-import Spinner from '../loading/Spinner';
 import { useBirderPage } from './BirderHooks';
+import { LoadingOverlay } from 'loading/LoadingOverlay';
 
 export function BirderPageContainer({ birderId }) {
   const { data, loading } = useBirderPage(birderId);
   if (loading) {
-    return <div><Spinner /></div>;
+    return <div><LoadingOverlay /></div>;
   }
   return <BirderPage data={data} />;
 }
