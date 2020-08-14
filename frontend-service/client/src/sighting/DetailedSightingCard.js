c0dead0ee935f68581bb37a28fe3a9427febd8b8
Lukas J. Wensby <wensby@gmail.com>
2020-08-14 21:41:09 +0200

    Redesign DetailedSightingCard

--- frontend-service/client/src/sighting/DetailedSightingCard.js
+++ frontend-service/client/src/sighting/DetailedSightingCard.js
@@ -1,19 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import { BirdCardName } from '../bird/BirdCardName';
-import { useBird } from '../bird/BirdHooks';
+import { useBird, useCommonName } from '../bird/BirdHooks';
 import { BirdThumbnailImage } from '../bird/BirdThumbnailImage';
 import './DetailedSightingCard.scss';
+import { BirdLink } from 'bird/BirdLink';
 
 export const DetailedSightingCard = ({ sighting }) => {
+  const { bird } = useBird(sighting.birdId);
+  const { commonName } = useCommonName(bird);
   return (
-    <div className='detailed-sighting-card card'>
-      <CardHeader sighting={sighting} />
-      <CardBody sighting={sighting} />
+    <div className='sighting-card'>
+      <BackgroundImage bird={bird} />
+      <Picture sighting={sighting} />
+      <Name name={commonName} />
+      <Body>
+        {sighting.position && sighting.position.name && <div>{sighting.position.name}</div>}
+      </Body>
+      <Moment date={sighting.date} time={sighting.time} />
     </div>
   );
 };
 
-const CardHeader = ({ sighting }) => {
+const Moment = ({date, time}) => {
+return <div className='date'>{time ? `${date} - ${time}` : date}</div>
+}
+
+const BackgroundImage = ({ bird }) => {
+  const [style, setStyle] = useState({});
+
+  useEffect(() => {
+    if (bird && bird.cover) {
+      setStyle({ backgroundImage: `url(${bird.cover.url})` });
+    }
+  }, [bird]);
+
+  return (
+    <>
+      <div className='background picture' style={style} />
+      <div className='background body-background' style={style} />
+      <div className='background gradient' />
+    </>
+  );
+};
+
+const Body = ({ children }) => {
+  return <div class='body'>{children}</div>;
+}
+
+const Name = ({ name }) => {
+  return <div class='name'>{name}</div>;
+};
+
+const Picture = ({ sighting }) => {
   const { bird } = useBird(sighting.birdId);
   const [style, setStyle] = useState({});
 
@@ -28,28 +65,10 @@ const CardHeader = ({ sighting }) => {
   }
 
   return (
-    <div className='card-header'>
-      <div className='background' style={style} />
+    <div className='picture'>
       <div className='middle-front'>
         <BirdThumbnailImage bird={bird} />
       </div>
-    </div>
-  );
-};
-
-const CardBody = ({ sighting }) => {
-  const { bird } = useBird(sighting.birdId);
-
-  if (!bird) {
-    return null;
-  }
-
-  return (
-    <div className='card-body'>
-      <BirdCardName bird={bird} />
-      <div>{sighting.date}</div>
-      <div>{sighting.time}</div>
-      {sighting.position && sighting.position.name && <div>{sighting.position.name}</div>}
     </div>
   );
 };
