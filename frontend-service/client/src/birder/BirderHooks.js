4c92c828e99b6a218bb7b15298c9e3d0e206bee6
Lukas J. Wensby <wensby@gmail.com>
2021-03-05 18:47:55 +0100

    Remove unused import

--- frontend-service/client/src/birder/BirderHooks.js
+++ frontend-service/client/src/birder/BirderHooks.js
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import birderRepository from './BirderRepository';
 import axios from 'axios';
 
