63233d7917e611bbb22bb6a45dd75d2ac9ce828b
Lukas J. Wensby <wensby@gmail.com>
2021-03-12 21:28:37 +0100

    Remove loading state from RoutedBirdPage

--- frontend-service/client/src/pages/RoutedBirdPage.js
+++ frontend-service/client/src/pages/RoutedBirdPage.js
@@ -1,20 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { BirdPage } from 'bird/BirdPage.js';
 import { Redirect } from 'react-router';
-import { useTitle } from 'specific/TitleContext';
 import axios from 'axios';
 
 export default ({ match }) => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setLoading(true);
     axios.get(`/api/bird-pages/${match.params.birdId}`)
       .then(response => response.data)
       .then(json => {
-        setLoading(false);
         setData(json);
       })
       .catch(error => setError(error));
